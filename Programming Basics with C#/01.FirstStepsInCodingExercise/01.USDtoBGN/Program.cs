49bdb9d01b561f44f77d822e9f53397a8e4b1dda
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-08 01:34:57 +0200

    Tast from the topic First steps in programming

---
+++ Programming Basics with C#/01.FirstStepsInCodingExercise/01.USDtoBGN/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.USDtoBGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            decimal usd = decimal.Parse(Console.ReadLine());
+            // 1 usd = 1.79549
+            decimal course = 1.79549M;
+            decimal bgn = usd * course;
+            Console.WriteLine(bgn);
+        }
+    }
+}
---
+++ Programming Basics with C#/01.FirstStepsInCodingExercise/07.FoodDelivery/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _07.FoodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal chickenMenu = 10.35M;
+            decimal fishMenu = 12.40M;
+            decimal vegetarianMenu = 8.15M;
+
+            decimal delivery = 2.50M;
+
+            int numberChickenMenus = int.Parse(Console.ReadLine());
+            int numberFishMenus = int.Parse(Console.ReadLine());
+            int numberVegetarianMenus = int.Parse(Console.ReadLine());
+
+            decimal priceChickenMenus = numberChickenMenus * chickenMenu;
+            decimal priceFishMenus = numberFishMenus * fishMenu;
+            decimal priceVegetarianMenus = numberVegetarianMenus * vegetarianMenu;
+
+            decimal totalSum = priceFishMenus + priceChickenMenus + priceVegetarianMenus;
+            decimal priceDessert = (totalSum * 0.20M);
+            totalSum = totalSum + priceDessert + delivery;
+
+            Console.WriteLine(Math.Round(totalSum, 2));
+
+
+        }
+    }
+}
