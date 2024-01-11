e4c75851a66b205e4bf4890124dc616f2ccf2639
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-11 15:20:06 +0200

    Task from the topic Conditional statements

---
+++ Programming Basics with C#/02.ConditionalStatementsLab/04.PasswordGuess/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04.PasswordGuess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "s3cr3t!P@ssw0rd";
+
+            string inputPassword = Console.ReadLine();
+            if (password == inputPassword)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/02.ConditionalStatementsLab/07.AreaOfFigures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _07.AreaOfFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            double S = 0;
+
+            if (type == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                S = a * a;
+            }
+            else if (type == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                S = a * b;
+            }
+            else if (type == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                S = Math.PI * (r * r);
+            }
+            else if (type == "triangle")
+            {
+                double c = double.Parse((Console.ReadLine()));
+                double h = double.Parse(((Console.ReadLine())));
+                S = (c * h) / 2;
+            }
+
+            Console.WriteLine($"{S:f3}");
+        }
+    }
+}
