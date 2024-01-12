ece7377ba3781274083725037541ee0a805c0837
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-13 01:53:57 +0200

    Last task from the topic Conditional statements advanced

---
+++ Programming Basics with C#/03.ConditionalStatementsAdvancedLab/12.TradeCommissions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12.TradeCommissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
