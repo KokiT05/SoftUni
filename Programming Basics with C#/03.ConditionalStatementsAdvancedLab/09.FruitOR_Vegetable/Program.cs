5cbba5fccb48115cb036bdd8b1f5f2df162ad44b
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-12 17:06:04 +0200

    More tasks from the topic Conditional statements advanced

---
+++ Programming Basics with C#/03.ConditionalStatementsAdvancedLab/09.FruitOR_Vegetable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.FruitOR_Vegetable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
