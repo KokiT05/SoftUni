31f95e97f6d9aa31ae7d503996e0640476273a21
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-13 16:50:15 +0200

    New exercise from the topic Conditional statements advanced

--- Programming Basics with C#/03.ConditionalStatementsAdvancedExercise.sln/04.FishingBoat/Program.cs
+++ Programming Basics with C#/03.ConditionalStatementsAdvancedExercise.sln/04.FishingBoat/Program.cs
@@ -6,7 +6,63 @@ namespace _04.FishingBoat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double shipPriceSpring = 3000;
+            double shipPriceSummerAutumn = 4200;
+            double shipPriceWinter = 2600;
+            double discount = 0;
+            double price = 0;
+
+            //int group = int.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int numberOfFishermen = int.Parse(Console.ReadLine());
+
+            season = season.ToLower();
+
+            if (season == "spring")
+            {
+                price = shipPriceSpring;
+            }
+            else if (season == "summer" || season == "autumn")
+            {
+                price = shipPriceSummerAutumn;
+            }
+            else if (season == "winter")
+            {
+                price = shipPriceWinter;
+            }
+
+            if (numberOfFishermen > 0 && numberOfFishermen <= 6)
+            {
+                discount = 0.10;
+            }
+            else if (numberOfFishermen >= 7 && numberOfFishermen <= 11)
+            {
+                discount = 0.15;
+            }
+            else if (numberOfFishermen >= 12)
+            {
+                discount = 0.25;
+            }
+
+            price = price - (price * discount);
+
+            if (numberOfFishermen % 2 == 0 && season != "autumn")
+            {
+                discount =  0.05;
+                price = price - (price * discount);
+            }
+
+            double result = Math.Abs(price - budget);
+
+            if (budget >= price)
+            {
+                Console.WriteLine($"Yes! You have {result:f2} leva left.");
+            }
+            else 
+            {
+                Console.WriteLine($"Not enough money! You need {result:f2} leva.");
+            }
         }
     }
 }
--- Programming Basics with C#/03.ConditionalStatementsAdvancedExercise.sln/05.Journey/Program.cs
+++ Programming Basics with C#/03.ConditionalStatementsAdvancedExercise.sln/05.Journey/Program.cs
@@ -6,7 +6,52 @@ namespace _05.Journey
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double precentageAmountSpent = 0;
+            string destination = string.Empty;
+            string rest = string.Empty;
+
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            if (budget >= 0 && budget <= 100)
+            {
+                destination = "Bulgaria";
+                if (season == "summer")
+                {
+                    rest = "Camp";
+                    precentageAmountSpent = 0.30;
+                }
+                else if (season == "winter")
+                {
+                    rest = "Hotel";
+                    precentageAmountSpent = 0.70;
+                }
+            }
+            else if (budget <= 1000)
+            {
+                destination = "Balkans";
+                if (season == "summer")
+                {
+                    rest = "Camp";
+                    precentageAmountSpent = 0.40;
+                }
+                else if (season == "winter")
+                {
+                    rest = "Hotel";
+                    precentageAmountSpent = 0.80;
+                }
+            }
+            else if (budget > 1000)
+            {
+                rest = "Hotel";
+                destination = "Europe";
+                precentageAmountSpent = 0.90;
+            }
+
+            double price = budget * precentageAmountSpent;
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{rest} - {price}");
         }
     }
 }
