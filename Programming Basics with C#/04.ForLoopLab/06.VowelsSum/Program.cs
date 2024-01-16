78c1b3b88d1ccd3e3728e10e8928d23c304a2c55
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-16 08:54:24 +0200

    New 4 task from the topic For loop

---
+++ Programming Basics with C#/04.ForLoopLab/06.VowelsSum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06.VowelsSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int e = 2;
+            int i = 3;
+            int o = 4;
+            int u = 5;
+            int sum = 0;
+
+            string text = Console.ReadLine();
+            for (int j = 0; j < text.Length; j++)
+            {
+                char letter = text[j];
+                switch (letter)
+                {
+                    case 'a':
+                        sum += a;
+                        break;
+                    case 'e':
+                        sum += e;
+                        break;
+                    case 'i':
+                        sum += i;
+                        break;
+                    case 'o':
+                        sum += o;
+                        break;
+                    case 'u':
+                        sum += u;
+                        break;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
