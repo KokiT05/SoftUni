47ec01e6be81db0aa6c60732649d1cbb4bfa604a
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-17 22:36:15 +0200

    New 2 tasks from the topic For loop exercise

---
+++ Programming Basics with C#/04.ForLoopExercise/05.Salary/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace _05.Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double facebookA_Fine = 150;
+            double instagramA_Fine = 100;
+            double redditA_Fine = 50;
+
+            string websiteName = string.Empty;
+            bool flag = true;
+
+            int numberOfOpenTabs = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= numberOfOpenTabs; i++)
+            {
+                websiteName = Console.ReadLine();
+
+                websiteName = websiteName.ToLower();
+
+                if (websiteName == "facebook")
+                {
+                    salary = salary - facebookA_Fine;
+                }
+                else if (websiteName == "instagram")
+                {
+                    salary = salary - instagramA_Fine;
+                }
+                else if (websiteName == "reddit")
+                {
+                    salary = salary - redditA_Fine;
+                }
+
+                if (salary <= 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (!flag)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/04.ForLoopExercise/06.Oscars/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06.Oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nomination = 1250.5;
+            double totalPoints = 0;
+            bool flag = true;
+
+
+            string actorName = Console.ReadLine();
+            double academyPoints = double.Parse(Console.ReadLine());
+            int numberOfAssesors = int.Parse(Console.ReadLine());
+
+            totalPoints = totalPoints + academyPoints;
+
+            for (int i = 1; i < numberOfAssesors; i++)
+            {
+                string appraiserName = Console.ReadLine();
+                double pointsFromEvaluator = double.Parse(Console.ReadLine());
+
+                totalPoints = totalPoints + ((appraiserName.Length * pointsFromEvaluator) / 2);
+
+                if (totalPoints > nomination)
+                {
+                    flag = false;
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {totalPoints:f1}");
+                }
+            }
+
+            if (!flag)
+            {
+                Console.WriteLine($"Sorry, {actorName} you need {totalPoints - nomination} more!");
+            }
+        }
+    }
+}
