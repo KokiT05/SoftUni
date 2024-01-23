de88e2b97918d72f8c2bdf2390cc9c1ba2edc325
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-23 23:05:51 +0200

    New task from topic While loop exercise (task 6)

---
+++ Programming Basics with C#/05.WhileLoopExercise/06.Cake/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace _06.Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isLeftCake = true;
+
+            double width = double.Parse(Console.ReadLine());
+            double length = double.Parse(Console.ReadLine());
+
+            double cakeS = width * length;
+
+            string pieceSize = Console.ReadLine();
+
+            while (pieceSize != "STOP")
+            {
+                cakeS -= double.Parse(pieceSize);
+
+                if (cakeS < 0)
+                {
+                    isLeftCake = false;
+                    break;
+                }
+
+                pieceSize = Console.ReadLine();
+            }
+
+            if (isLeftCake)
+            {
+                Console.WriteLine($"{cakeS} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(cakeS)} pieces more.");
+            }
+        }
+    }
+}
