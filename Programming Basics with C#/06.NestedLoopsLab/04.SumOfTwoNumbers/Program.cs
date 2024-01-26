39f982a7f5e4d7cb0383af9c0fb1562fa524ad2d
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-26 12:17:11 +0200

    New 2 task from the topic Nested loops lab

---
+++ Programming Basics with C#/06.NestedLoopsLab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _04.SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCombinations = 0;
+            bool isFound = false;
+
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    numberOfCombinations++;
+
+                    if (i + j == magicNumber)
+                    {
+                        start = i;
+                        end = j;
+                        isFound = true;
+                        break;
+                    }
+                }
+
+                if (isFound == true)
+                {
+                    break;
+                }
+            }
+
+            if (isFound)
+            {
+                Console.WriteLine($"Combination N:{numberOfCombinations} ({start} + {end} = {magicNumber})");
+            }
+            else
+            {
+                Console.WriteLine($"{numberOfCombinations} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/06.NestedLoopsLab/05.Travelling/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double savedMoney = 0;
+            double sum = 0;
+
+            string destination = Console.ReadLine();
+            double price = double.Parse(Console.ReadLine());
+
+            while (destination != "End")
+            {
+                for (int i = 0; i < price; i++)
+                {
+                    savedMoney = double.Parse(Console.ReadLine());
+                    sum += savedMoney;
+
+                    if (sum >= price)
+                    {
+                        Console.WriteLine($"Going to {destination}");
+                        break;
+                    }
+                }
+
+                destination = Console.ReadLine();
+                price = double.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
