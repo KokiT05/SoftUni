bac2ffb06d6183e568bbfc755275e924f3d57429
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-28 13:11:01 +0200

    Last 2 tasks from the topic Nested loops exercise

---
+++ Programming Basics with C#/06.NestedLoopsExercise/05.SpecialNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Formats.Asn1;
+
+namespace _05.SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isSpecial = false;
+            int number = 0;
+
+            int unit = 0;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1111; i <= 9999; i++)
+            {
+                number = i;
+                for (int j = 0; j < 4; j++)
+                {
+
+                    unit = number % 10;
+                    number = number / 10;
+
+                    if (unit != 0)
+                    {
+                        if (n % unit == 0)
+                        {
+                            isSpecial = true;
+                        }
+                        else
+                        {
+                            isSpecial = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+                }
+
+                if (isSpecial)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/06.NestedLoopsExercise/06.CinemaTickets/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace _06.CinemaTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalStudentSeats = 0;
+            double totalKidSeats = 0;
+            double totalStandardSeats = 0;
+
+            int totalTickets = 0;
+
+            double precentFilled = 0;
+            double occupiedSeats = 0;
+
+            double percentStudentTickets = 0;
+            double percentKidTickets = 0;
+            double percentStandardTickets = 0;
+
+            int freeSeats = 0;
+            string command = string.Empty;
+
+            string moviName = Console.ReadLine();
+
+            while (moviName != "Finish")
+            {
+                freeSeats = int.Parse(Console.ReadLine());
+
+                precentFilled = 0;
+                occupiedSeats = 0;
+
+                for (int i = 1; i <= freeSeats; i++)
+                {
+                    if (freeSeats <= 0)
+                    {
+                        break;
+                    }
+
+                    command = Console.ReadLine();
+
+                    if (command == "End")
+                    {
+                        break;
+                    }
+                    else if (command == "standard")
+                    {
+                        totalStandardSeats++;
+                        occupiedSeats++;
+                    }
+                    else if (command == "kid")
+                    {
+                        totalKidSeats++;
+                        occupiedSeats++;
+                    }
+                    else if (command == "student")
+                    {
+                        totalStudentSeats++;
+                        occupiedSeats++;
+                    }
+
+                    totalTickets++;
+                }
+                precentFilled = (occupiedSeats / freeSeats) * 100;
+                Console.WriteLine($"{moviName} - {precentFilled:f2}% full.");
+            }
+            percentKidTickets = totalKidSeats / totalTickets;
+            percentStandardTickets = totalStandardSeats / totalTickets;
+            percentStudentTickets = totalStudentSeats / totalTickets;
+
+
+            Console.WriteLine($"Total tickets: {totalTickets}");
+            Console.WriteLine($"{percentStudentTickets:f2} student tickets.");
+            Console.WriteLine($"{percentStandardTickets:f2} standard tickets.");
+            Console.WriteLine($"{percentKidTickets:f2} kid tickets.");
+
+        }
+    }
+}
