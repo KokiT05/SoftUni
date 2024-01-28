05b648510330ecfddb37eaed58504356e615bbdb
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-28 08:35:05 +0200

    Add new topic Nested loops exercise

---
+++ Programming Basics with C#/06.NestedLoopsExercise/01.NumberPyramid/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int current = 0;
+            bool isBigger = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (current == n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+
+                    Console.Write($"{++current} ");
+                }
+
+                if (isBigger)
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/06.NestedLoopsExercise/02.EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _02.EqualSumsEvenOddPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int unit = 0;
+            int current = 0;
+
+            int sumOddPosition = 0;
+            int sumEvenPosition = 0;
+
+
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                current = i;
+                sumEvenPosition = 0;
+                sumOddPosition = 0;
+                for (int j = 6; j > 0; j--)
+                {
+                    unit = current % 10;
+
+                    if (j % 2 == 0)
+                    {
+                        sumEvenPosition += unit;
+                    }
+                    else
+                    {
+                        sumOddPosition += unit;
+                    }
+
+                    current = current / 10;
+                }
+
+                if (sumOddPosition == sumEvenPosition)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
