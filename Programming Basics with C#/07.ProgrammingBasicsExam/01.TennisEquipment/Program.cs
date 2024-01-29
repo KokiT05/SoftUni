bfe155997e378007975484bfe6ff076814092e62
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-29 05:28:12 +0200

    New task from the topic Programming Baisics Exam task: 1

--- Programming Basics with C#/07.ProgrammingBasicsExam/01.TennisEquipment/Program.cs
+++ Programming Basics with C#/07.ProgrammingBasicsExam/01.TennisEquipment/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double priceOfOneTennisRacket = double.Parse(Console.ReadLine());
+            int NumberOfTennisRackets = int.Parse(Console.ReadLine());
+            int numberOfPairsOfSneakers = int.Parse(Console.ReadLine());
+            
+
+            double finalPriceForRockets = priceOfOneTennisRacket * NumberOfTennisRackets;
+            double finalPriceForSneakers = (priceOfOneTennisRacket * 1 / 6.00) * numberOfPairsOfSneakers;
+
+            double totalPrice = (finalPriceForRockets + finalPriceForSneakers) * 0.20 ;
+
+            double result = finalPriceForRockets + finalPriceForSneakers + totalPrice;
+
+            double priceForDjokovic = result / 8;
+            double priceForSponsors = result * (7 / 8.00);
+
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(priceForDjokovic)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(priceForSponsors)}");
         }
     }
 }
