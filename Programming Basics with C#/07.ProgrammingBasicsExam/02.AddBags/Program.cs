4846fbfe9ecd382158d77ea9bee5816def3377b4
Konstantin Tinev <konstantintinev5@gmail.com>
2024-01-30 21:24:13 +0200

    Add new tasks from the topic Programming Basics Exam (task: 2, task: 3)

---
+++ Programming Basics with C#/07.ProgrammingBasicsExam/02.AddBags/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _02.AddBags
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 0;
+            double totalPrice = 0;
+
+            double baggageCost = double.Parse(Console.ReadLine());
+            double kilogramsOfBaggage = double.Parse((Console.ReadLine()));
+            int days = int.Parse(Console.ReadLine());
+            int numberOfBags = int.Parse(Console.ReadLine());
+
+            if (kilogramsOfBaggage < 10)
+            {
+                price = baggageCost * 0.20;
+            }
+            else if (kilogramsOfBaggage >= 10 && kilogramsOfBaggage <= 20)
+            {
+                price = baggageCost * 0.50;
+            }
+            else if (kilogramsOfBaggage >= 20)
+            {
+                price = baggageCost;
+            }
+
+            if (days > 30)
+            {
+                price = price + (price * 0.10);
+            }
+            else if (days >= 7 && days <= 30)
+            {
+                price = price + (price * 0.15);
+            }
+            else if (days < 7 && days > 0)
+            {
+                price = price + (price * 0.40);
+            }
+
+            totalPrice = price * numberOfBags;
+
+            Console.WriteLine($"The total price of bags is: {totalPrice:f2} lv.");
+        }
+    }
+}
---
+++ Programming Basics with C#/07.ProgrammingBasicsExam/03.OscarsWeekINCinema/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace _03.OscarsWeekINCinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 0;
+            double totalPrice = 0;
+
+            string movieName = Console.ReadLine();
+            string typeOfHall = Console.ReadLine();
+            int numberOfTicketsPurchased = int.Parse(Console.ReadLine());
+
+            if (movieName == "A Star Is Born")
+            {
+                if (typeOfHall == "normal")
+                {
+                    price = 7.50;
+                }
+                else if (typeOfHall == "luxury")
+                {
+                    price = 10.50;
+                }
+                else if (typeOfHall == "ultra luxury")
+                {
+                    price = 13.50;
+                }
+            }
+            else if (movieName == "Bohemian Rhapsody")
+            {
+                if (typeOfHall == "normal")
+                {
+                    price = 7.35;
+                }
+                else if (typeOfHall == "luxury")
+                {
+                    price = 9.45;
+                }
+                else if (typeOfHall == "ultra luxury")
+                {
+                    price = 12.75;
+                }
+            }
+            else if (movieName == "Green Book")
+            {
+                if (typeOfHall == "normal")
+                {
+                    price = 8.15;
+                }
+                else if (typeOfHall == "luxury")
+                {
+                    price = 10.25;
+                }
+                else if (typeOfHall == "ultra luxury")
+                {
+                    price = 13.25;
+                }
+            }
+            else if (movieName == "The Favourite")
+            {
+                if (typeOfHall == "normal")
+                {
+                    price = 8.75;
+                }
+                else if (typeOfHall == "luxury")
+                {
+                    price = 11.55;
+                }
+                else if (typeOfHall == "ultra luxury")
+                {
+                    price = 13.95;
+                }
+            }
+
+            totalPrice = price * numberOfTicketsPurchased;
+
+            Console.WriteLine($"A Star Is Born -> {totalPrice:f2}");
+        }
+    }
+}
