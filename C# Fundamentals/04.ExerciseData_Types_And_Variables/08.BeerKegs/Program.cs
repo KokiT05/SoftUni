9569de64800d83330661d2a7efb97a71518afcb0
Konstantin Tinev <konstantintinev5@gmail.com>
2024-02-19 20:45:50 +0200

    Add new exercise (ex 8, ex 9)

---
+++ C# Fundamentals/04.ExerciseData_Types_And_Variables/08.BeerKegs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _08.BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string model = string.Empty;
+            double radius = 0.0;//double.Parse(Console.ReadLine());
+            int height = 0;//int.Parse(Console.ReadLine());
+            double volume = 0;
+            double maxVolume = 0;
+            string biggestKeg = string.Empty;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                model = Console.ReadLine();
+                radius = double.Parse(Console.ReadLine());
+                height = int.Parse(Console.ReadLine());
+
+                volume = Math.PI * Math.Pow(radius,2) * height;
+
+                if (volume >= maxVolume)
+                {
+                    maxVolume = volume;
+                    biggestKeg = model;
+                }
+            }
+
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
