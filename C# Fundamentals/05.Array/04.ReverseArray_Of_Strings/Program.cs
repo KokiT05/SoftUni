fd757fae4b2095fe4fa50d3ff9c2bca3bd020ae7
Konstantin Tinev <konstantintinev5@gmail.com>
2024-03-17 22:52:05 +0200

    Add new tasks, task 5 Sum even numbers, task 6 Even and odd subtraction

--- C# Fundamentals/05.Array/04.ReverseArray_Of_Strings/Program.cs
+++ C# Fundamentals/05.Array/04.ReverseArray_Of_Strings/Program.cs
@@ -7,7 +7,24 @@ namespace _04.ReverseArray_Of_Strings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(string.Join(' ', Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse()));
+            //Mathod 1
+            //Console.WriteLine(string.Join(' ', Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse()));
+
+            string input = Console.ReadLine();
+            string[] stringArray = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string olderChar = " ";
+            for (int i = 0; i < stringArray.Length / 2; i++)
+            {
+                olderChar = stringArray[i];
+                stringArray[i] = stringArray[stringArray.Length - 1 - i];
+                stringArray[stringArray.Length - 1 - i] = olderChar;
+            }
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                Console.Write($"{stringArray[i]} ");
+            }
         }
     }
 }
---
+++ C# Fundamentals/05.Array/05.SumEvenNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SumEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int[] numbers = input
+                            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
+
+            int sum = 0;
+            for (int i = 1; i <= numbers.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += numbers[i - 1];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
