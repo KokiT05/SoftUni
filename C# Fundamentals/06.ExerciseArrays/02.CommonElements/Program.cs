76214ee8693c488e6a0052c74ced18ae3e3fcf99
Konstantin Tinev <konstantintinev5@gmail.com>
2024-03-18 17:15:07 +0200

    New topic: 06.ExerciseArrays

---
+++ C# Fundamentals/06.ExerciseArrays/02.CommonElements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _02.CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInputArray = Console
+                                        .ReadLine()
+                                        .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] secondInputArray = Console
+                                        .ReadLine()
+                                        .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string commonString = string.Empty;
+
+            string currentStringFirstArray = string.Empty;
+            string currentStringSecondArray = string.Empty;
+
+            for (int i = 0; i < secondInputArray.Length; i++)
+            {
+                currentStringSecondArray = secondInputArray[i];
+
+                for (int j = 0; j < firstInputArray.Length; j++)
+                {
+                    currentStringFirstArray = firstInputArray[j];
+
+                    if (currentStringFirstArray == currentStringSecondArray)
+                    {
+                        commonString += " " + secondInputArray[i];
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{commonString.TrimStart()}");
+        }
+    }
+}
---
+++ C# Fundamentals/06.ExerciseArrays/06.ExerciseArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06.ExerciseArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfWagons = int.Parse(Console.ReadLine());
+
+            int[] wagons = new int[countOfWagons];
+            int currentWagon = 0;
+            for (int i = 0; i < countOfWagons; i++)
+            {
+                currentWagon = int.Parse(Console.ReadLine());
+                wagons[i] = currentWagon;
+            }
+
+            int sumOfPeople = 0;
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                sumOfPeople += wagons[i];
+            }
+
+            Console.WriteLine(string.Join(' ', wagons));
+            Console.WriteLine(sumOfPeople);
+        }
+    }
+}
