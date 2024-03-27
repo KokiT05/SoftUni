36f617624decbb53c6fcb2d16dcda9f65be59ed0
Konstantin Tinev <konstantintinev5@gmail.com>
2024-03-27 13:27:19 +0200

    New task from More Exercise Array, taks 3 and task 4

---
+++ C# Fundamentals/06.ExerciseArrays/03.3RecursiveFibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _03._3RecursiveFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(SolveMemoization(n));
+            //FibonachiRecursion(n);
+            //Console.WriteLine(n);
+        }
+
+        public static double SolveMemoization(int n)
+        {
+            double result;
+            if (memo.ContainsKey(n))
+            {
+                result = memo[n];
+            }
+            else
+            {
+                result = SolveMemoization(n - 1) + SolveMemoization(n - 2);
+                memo[n] = result;
+            }
+            return result;
+
+        }
+
+        static Dictionary<int, double> memo = new Dictionary<int, double>() {
+            {1, 1},
+            {2, 1}
+        };
+
+        //static long FibonachiRecursion(int n)
+        //{
+        //    if (n <= 2)
+        //    {
+        //        return 1;
+        //    }
+        //    return FibonachiRecursion(n - 1) + FibonachiRecursion(n - 2);
+        //}
+    }
+}
---
+++ C# Fundamentals/06.ExerciseArrays/04.4FoldАndSum/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _04._4FoldАndSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console
+                            .ReadLine()
+                            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
+
+            int k = numbers.Length / 4;
+
+            int[] firstSide = new int[k];
+            int[] secondSide = new int[k];
+            int[] result = new int[numbers.Length / 2];
+
+            for (int i = 0; i < k; i++)
+            {
+                firstSide[i] = numbers[i];
+                numbers[i] = 0;
+            }
+
+            int startIndex = 0;
+            for (int i = numbers.Length - 1; i > numbers.Length - (k + 1); i--)
+            {
+                int number = numbers[i];
+                numbers[i] = 0;
+                secondSide[startIndex] = number;
+                startIndex++;
+            }
+            int index = k;
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = numbers[index];
+                index++;
+            }
+
+            Array.Reverse(firstSide);
+            
+
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write($"{firstSide[i] + result[i]} ");
+            }
+
+            for (int i = k; i > 0; i--)
+            {
+                Console.Write($"{secondSide[secondSide.Length - i] + result[result.Length - i]} ");
+            }
+        }
+    }
+}
