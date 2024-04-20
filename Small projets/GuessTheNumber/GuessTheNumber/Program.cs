e49238ec271254af29acd899beddfb61bba4a3b1
Konstantin Tinev <konstantintinev5@gmail.com>
2024-04-20 22:50:30 +0300

    Add new small project

---
+++ Small projets/GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace GuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, guest the number :)");
+            Console.WriteLine("For exit enter 0");
+
+            Console.Write("Enter your number [1, 10]: ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            
+
+            Random randomNumber = new Random();
+            List<int> randomNumbers = new List<int>();
+            int currentNumber = 0;
+
+            while (inputNumber != 0)
+            {
+                currentNumber = randomNumber.Next(1, 11);
+                if (inputNumber == currentNumber)
+                {
+                    Console.WriteLine("You win!");
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Incorrect!");
+                    Console.WriteLine($"Your number is: {inputNumber}");
+                    Console.WriteLine($"The magic number was: {currentNumber}. Good luck next time :)");
+                }
+
+                Console.Write("Enter your number [1, 10]: ");
+                inputNumber = int.Parse(Console.ReadLine());
+
+                randomNumbers.Add(currentNumber);
+            }
+        }
+    }
+}
