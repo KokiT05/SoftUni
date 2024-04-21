a1c0136a509f38e5d866f3d5776b37ef9fdb3138
Konstantin Tinev <konstantintinev5@gmail.com>
2024-04-21 23:23:40 +0300

    New task: 7 AppendArray

--- C# Fundamentals/11ExerciseListss/7.AppendArrays/Program.cs
+++ C# Fundamentals/11ExerciseListss/7.AppendArrays/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] inputData = Console.ReadLine().Split('|').Reverse().ToArray();
+            List<string> pieces = new List<string>(inputData.Length);
+            List<string> result = new List<string>(pieces.Count);
+
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                string[] numbers = inputData[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    result.Add(numbers[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join("", result));
         }
     }
 }
