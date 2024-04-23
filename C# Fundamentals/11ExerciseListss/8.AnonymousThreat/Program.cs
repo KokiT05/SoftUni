b0f5331f7905a9c283bd6cc9d705ea68dbd17df3
Konstantin Tinev <konstantintinev5@gmail.com>
2024-04-23 23:39:48 +0300

    Fix bugs: taks 9. task 8

--- C# Fundamentals/11ExerciseListss/8.AnonymousThreat/Program.cs
+++ C# Fundamentals/11ExerciseListss/8.AnonymousThreat/Program.cs
@@ -1,20 +1,24 @@
-﻿namespace _8.AnonymousThreat
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P08.AnonymousThreat
 {
-    internal class Program
+    internal class AnonymousThreat
     {
         static void Main(string[] args)
         {
             List<string> strings = Console.ReadLine()
-                                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                                    .ToList();
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
 
             string currentOutput = Console.ReadLine();
 
             while (currentOutput != "3:1")
             {
                 List<string> currentCommand = currentOutput
-                                            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                                            .ToList();
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
 
                 if (currentCommand[0] == "merge")
                 {
@@ -25,13 +29,12 @@
                     {
                         startIndex = 0;
                     }
-
-                    if (endIndex >= strings.Count)
+                    if (endIndex > strings.Count - 1)
                     {
                         endIndex = strings.Count - 1;
                     }
 
-                    MergeStrings(strings, startIndex, endIndex);
+                    MerageStrings(strings, startIndex, endIndex);
                 }
                 else if (currentCommand[0] == "divide")
                 {
@@ -43,17 +46,8 @@
 
             Console.WriteLine(string.Join(' ', strings));
         }
-        static void MergeStrings(List<string> strings, int startIndex, int endIndex)
-        {
-            // 11 222 444
-            for (int i = startIndex + 1; i <= endIndex; i++)
-            {
-                strings[startIndex] += strings[startIndex + 1];
-                strings.RemoveAt(startIndex + 1);
-            }
-        }
 
-        static void DivideStrings(List<string> strings, List<string> currentCommand)
+        private static void DivideStrings(List<string> strings, List<string> currentCommand)
         {
             int index = int.Parse(currentCommand[1]);
             int partition = int.Parse(currentCommand[2]);
@@ -81,19 +75,29 @@
             else
             {
                 lengthOfSubstrings = lengthOfSubstrings / partition <= 0 ? 1 : lengthOfSubstrings / partition;
-                int countOfSubstring = partition;
+
+                int countOfSubstrings = partition;
 
                 for (int currentSymbol = 0; currentSymbol < currentString.Length; currentSymbol++)
                 {
-                    if (currentSymbol != 0 && currentSymbol % lengthOfSubstrings == 0 && countOfSubstring > 1)
+                    if (currentSymbol != 0 && currentSymbol % lengthOfSubstrings == 0 && countOfSubstrings > 1)
                     {
                         index++;
                         strings.Insert(index, string.Empty);
-                        countOfSubstring--;
+                        countOfSubstrings--;
                     }
 
                     strings[index] += currentString[currentSymbol];
                 }
+            }
+        }
+
+        private static void MerageStrings(List<string> strings, int startIndex, int endIndex)
+        {
+            for (int j = startIndex + 1; j <= endIndex; j++)
+            {
+                strings[startIndex] += strings[startIndex + 1];
+                strings.RemoveAt(startIndex + 1);
             }
         }
     }
