0f97e58a41b768e994713ce4655569788031dccc
Konstantin Tinev <konstantintinev5@gmail.com>
2024-04-25 21:54:22 +0300

    Fix tasks: 4, 5

--- C# Fundamentals/11ExerciseListss/4.Mixed_up_Lists/Program.cs
+++ C# Fundamentals/11ExerciseListss/4.Mixed_up_Lists/Program.cs
@@ -4,7 +4,65 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> firstNumbers = Console.ReadLine()
+                                            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                            .Select(int.Parse)
+                                            .ToList();
+
+            List<int> secondNumbers = Console.ReadLine()
+                                             .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                             .Select(int.Parse)
+                                             .ToList();
+
+            int maxCount = Math.Max(firstNumbers.Count, secondNumbers.Count);
+            int minCount = Math.Min(firstNumbers.Count, secondNumbers.Count);
+
+            List<int> result = new List<int>(maxCount);
+            for (int i = 0; i < minCount; i++)
+            {
+                result.Add(firstNumbers[i]);
+                result.Add(secondNumbers[secondNumbers.Count - 1 - i]);
+            }
+
+            int initialBoundary = 0;
+            int finalBoundary = 0;
+
+            if (firstNumbers.Count >= secondNumbers.Count)
+            {
+                initialBoundary = firstNumbers[firstNumbers.Count - 2];
+                finalBoundary = firstNumbers[firstNumbers.Count - 1];
+
+                if (firstNumbers[firstNumbers.Count - 1] > firstNumbers[firstNumbers.Count - 2])
+                {
+                    initialBoundary = firstNumbers[firstNumbers.Count - 2];
+                    finalBoundary = firstNumbers[firstNumbers.Count - 1];
+                }
+                else
+                {
+                    initialBoundary = firstNumbers[firstNumbers.Count - 1];
+                    finalBoundary = firstNumbers[firstNumbers.Count - 2];
+                }
+            }
+            else
+            {
+                initialBoundary = secondNumbers[0];
+                finalBoundary = secondNumbers[1];
+
+                if (secondNumbers[0] > secondNumbers[1])
+                {
+                    initialBoundary = secondNumbers[1];
+                    finalBoundary = secondNumbers[0];
+                }
+                else
+                {
+                    initialBoundary = secondNumbers[0];
+                    finalBoundary = secondNumbers[1];
+                }
+            }
+
+            result.Sort();
+
+            Console.WriteLine(string.Join(' ', result.Where(n => n > initialBoundary && n < finalBoundary)));
         }
     }
 }
--- C# Fundamentals/11ExerciseListss/5.DrumSet/Program.cs
+++ C# Fundamentals/11ExerciseListss/5.DrumSet/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double savings = double.Parse(Console.ReadLine());
+            int[] drumSet = Console.ReadLine()
+                                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                        .Select(int.Parse)
+                                        .ToArray();
+
+            List<int> copyDrumSet = new List<int>(drumSet.Length);
+            for (int i = 0; i < drumSet.Length; i++)
+            {
+                copyDrumSet.Add(drumSet[i]);
+            }
+
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "stop")
+            {
+                for (int i = 0; i < drumSet.Length; i++)
+                {
+                    drumSet[i] = drumSet[i] - 3;
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', copyDrumSet));
+            Console.WriteLine(string.Join(' ', drumSet));
+            //int[] copyDrumSet = new int[drumSet.Length];
+            //drumSet.CopyTo(copyDrumSet, 0);
+
+            //string command = Console.ReadLine();
+            //while (command != "Hit it again, Gabsy!")
+            //{
+            //    int hitPower = int.Parse(command);
+            //    double drumPrice = 0;
+            //    for (int i = 0; i < drumSet.Length; i++)
+            //    {
+            //        drumPrice = copyDrumSet[i] * 3;
+            //        if (drumSet[i] - hitPower <= 0 && savings - drumPrice >= 0)
+            //        {
+            //            savings -= drumPrice;
+            //            drumSet[i] = copyDrumSet[i];
+            //        }
+            //        else if (drumSet[i] - hitPower <= 0 && savings - drumPrice < 0)
+            //        {
+            //            drumSet[i] = 0;
+            //        }
+            //        else
+            //        {
+            //            drumSet[i] -= hitPower;
+            //        }
+            //    }
+
+            //    command = Console.ReadLine();
+            //}
+
+            //Console.WriteLine(string.Join(' ', drumSet.Where(ds => ds > 0)));
+            //Console.WriteLine($"Gabsy has {savings:f2}lv.");
         }
     }
 }
