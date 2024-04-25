0f35ec70221ac86b55c5f69a4a3bd0d6dc6a50d5
Konstantin Tinev <konstantintinev5@gmail.com>
2024-04-25 17:41:52 +0300

    Last tasks

---
+++ C# Fundamentals/11ExerciseListss/5.DrumSet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5.DrumSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
