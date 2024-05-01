dc1930c8901de7475aba8590811f98bd9c34efcb
Konstantin Tinev <konstantintinev5@gmail.com>
2024-05-01 17:34:30 +0300

    Add new topic: Exerise Objects and Classes

---
+++ C# Fundamentals/12.ObjectsAndClassess/7.VehicleCatalogue/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace _7.VehicleCatalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Catalog catalog = new Catalog();
+
+            string command = Console.ReadLine();
+            while (command.ToLower() != "end")
+            {
+                string[] dataInformation = command.Split('/', StringSplitOptions.RemoveEmptyEntries);
+                CreateObject(dataInformation, catalog);
+                command = Console.ReadLine();
+            }
+
+            catalog.Trucks = catalog.Trucks.OrderBy(t => t.Brand).ToList();
+            catalog.Cars = catalog.Cars.OrderBy(c => c.Brand).ToList();
+
+            if (catalog.Cars.Count != 0)
+            {
+                PrintCar(catalog);
+            }
+
+            if (catalog.Trucks.Count != 0)
+            {
+                PrintTruck(catalog);
+            }
+        }
+
+        public static void CreateObject(string[] information, Catalog catalog)
+        {
+            string type = information[0];
+            string brand = information[1];
+            string model = information[2];
+            double horsePowerOrWeight = double.Parse(information[3]);
+
+            if (type.ToLower() == "car")
+            {
+                Car car = new Car(brand, model, horsePowerOrWeight);
+                catalog.Cars.Add(car);
+            }
+            else if (type.ToLower() == "truck")
+            {
+                Truck truck = new Truck(brand, model, horsePowerOrWeight);
+                catalog.Trucks.Add(truck);
+            }
+        }
+
+        public static void PrintTruck(Catalog catalog)
+        {
+            Console.WriteLine("Trucks:");
+            foreach (Truck truck in catalog.Trucks)
+            {
+                Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+            }
+        }
+
+        public static void PrintCar(Catalog catalog)
+        {
+            Console.WriteLine("Cars:");
+            foreach (Car car in catalog.Cars)
+            {
+                Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
+            }
+        }
+    }
+
+    public class Truck
+    {
+        public Truck(string brand, string model, double weight)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Weight = weight;
+        }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public double Weight { get; set; }
+    }
+
+    public class Car
+    {
+        public Car(string brand, string model, double horsePower)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.HorsePower = horsePower;
+        }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public double HorsePower { get; set; }
+    }
+
+    public class Catalog
+    {
+        public Catalog()
+        {
+            this.Cars = new List<Car>();
+            this.Trucks = new List<Truck>();
+        }
+
+        public List<Car> Cars { get; set;}
+
+        public List<Truck> Trucks { get; set;}
+    }
+}
---
+++ Small projets/13.ExerciseObjectsAndClasses/2.Articles/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _2.Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] currentData = Console.ReadLine()
+                                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                    .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Article article = new Article(currentData[0], currentData[1], currentData[2]);
+            for (int i = 0; i < n; i++)
+            {
+                currentData = Console.ReadLine()
+                                    .Split(": ", StringSplitOptions.RemoveEmptyEntries)
+                                    .ToArray();
+
+                string command = currentData[0];
+
+                if (command.ToLower() == "edit")
+                {
+                    article.Edint(currentData[1]);
+                }
+                else if (command.ToLower() == "changeauthor")
+                {
+                    article.ChangeAuthor(currentData[1]);
+                }
+                else if(command.ToLower() == "rename")
+                {
+                    article.Rename(currentData[1]);
+                }
+            }
+
+            Console.WriteLine(article);
+        }
+    }
+
+    public class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Author = author;
+        }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public void Edint(string content)
+        {
+            this.Content = content;
+        }
+
+        public void ChangeAuthor(string author)
+        {
+            this.Author = author;
+        }
+
+        public void Rename(string name)
+        {
+            this.Title = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+
+    }
+}
