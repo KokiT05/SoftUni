ce677b3f4d8a57eae26bf96a63ce36592fb2ecd3
Konstantin Tinev <konstantintinev5@gmail.com>
2024-05-20 15:00:33 +0300

    Mid exam, task: 1

---
+++ C# Fundamentals/15.ProgrammingFundamentalsMidExamRetake/01.Counter-Strike/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _01.Counter_Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            bool isEnoughtEnergy = true;
+            int wonBattleCount = 0;
+            string command = Console.ReadLine();
+            while (command != "End of battle")
+            {
+                int distance = int.Parse(command);
+
+                if ((energy - distance) < 0)
+                {
+                    isEnoughtEnergy = false;
+                    break;
+                }
+                else
+                {
+                    energy -= distance;
+                    wonBattleCount++;
+
+                    if (wonBattleCount % 3 == 0)
+                    {
+                        energy += wonBattleCount;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (isEnoughtEnergy)
+            {
+                Console.WriteLine($"Won battles: {wonBattleCount}. Energy left: {energy}");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough energy! Game ends with {wonBattleCount} won battles and {energy} energy.");
+            }
+        }
+    }
+}
