55161e7137fd25ccd6d6164bd35c8040f44e91f8
Konstantin Tinev <konstantintinev5@gmail.com>
2024-05-25 17:49:55 +0300

    New task 04.GuineaPig

---
+++ C# Fundamentals/15.ProgrammingFundamentalsMidExamRetake/04.GuineaPig/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _04.GuineaPig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double quantityFood = double.Parse(Console.ReadLine());
+            double quantityHal = double.Parse(Console.ReadLine());
+            double quantityCover = double.Parse(Console.ReadLine());
+            double guineaWeight = double.Parse(Console.ReadLine());
+
+            quantityFood = quantityFood * 1000;
+            quantityHal = quantityHal * 1000;
+            bool isEnought = true;
+
+            int count = 0;
+            while (count != 30)
+            {
+                count++;
+                quantityFood -= 300;
+
+                if (count % 2 == 0)
+                {
+                    quantityHal -= (quantityFood * 0.05);
+                }
+
+                if (count % 3 == 0)
+                {
+                    quantityCover = (quantityCover - (guineaWeight * 1 / 3));
+                }
+
+                if (quantityCover <= 0 || quantityFood <= 0 || quantityHal <= 0)
+                {
+                    isEnought = false;
+                    break;
+                }
+            }
+
+            if (isEnought)
+            {
+                Console.WriteLine("Everything is fine! Puppy is happy! " +
+                    $"Food: {quantityFood:f2}, Hay: {quantityHal:f2}, Cover: {quantityCover:f2}");
+            }
+            else
+            {
+                Console.WriteLine("Merry must go to the pet store!");
+            }
+        }
+    }
+}
