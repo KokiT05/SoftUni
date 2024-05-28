dfc3be869c81ee89f207ddb0ce4e8f1db379638d
Konstantin Tinev <konstantintinev5@gmail.com>
2024-05-28 14:14:59 +0300

    Add new tasks

---
+++ C# Fundamentals/17.AssociativeArraysExercise/01.CountCharsIn_a_String/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.CountCharsIn_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split(' ');
+
+            Dictionary<char, int> charactersCount = new Dictionary<char, int>();
+
+            foreach (string s in text)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (charactersCount.ContainsKey(s[i]))
+                    {
+                        charactersCount[s[i]]++;
+                    }
+                    else
+                    {
+                        charactersCount.Add(s[i], 1);
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<char, int> character in charactersCount)
+            {
+                Console.WriteLine($"{character.Key} -> {character.Value}");
+            }
+        }
+    }
+}
