0d0110fe8a214e48965640ea04522a7bad2f5cb6
Konstantin Tinev <konstantintinev5@gmail.com>
2024-05-28 07:56:42 +0300

    Solving new tasks, 02 Odd occurrences, 3 Word Synonyms

--- C# Fundamentals/16.AssociativeArrays/01.CountRealNumbers/Program.cs
+++ C# Fundamentals/16.AssociativeArrays/01.CountRealNumbers/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace _01.CountRealNumbers
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountRealNumbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> countOfNumbers = new SortedDictionary<double, int>();
+
+            foreach (double number in numbers) 
+            {
+                if (countOfNumbers.ContainsKey(number))
+                {
+                    countOfNumbers[number]++;
+                }
+                else
+                {
+                    countOfNumbers.Add(number, 1);
+                }
+            }
+
+            foreach (KeyValuePair<double, int> number in countOfNumbers)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
         }
     }
 }
---
+++ C# Fundamentals/16.AssociativeArrays/02.OddOccurrences/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ');
+
+            Dictionary<string, int> countOfWords = new Dictionary<string, int>();
+
+            foreach (string word in words) 
+            {
+                string wordInLower = word.ToLower();
+                if (countOfWords.ContainsKey(wordInLower))
+                {
+                    countOfWords[wordInLower]++;
+                }
+                else
+                {
+                    countOfWords.Add(wordInLower, 1);
+                }
+            }
+
+            foreach (string word in countOfWords.Keys) 
+            {
+                if (countOfWords[word] % 2 != 0)
+                {
+                    Console.Write($"{word} ");
+                }
+            }
+        }
+    }
+}
