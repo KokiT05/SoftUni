b73b41f4e6b60ebe902af00286a21860ac04db60
Konstantin Tinev <konstantintinev5@gmail.com>
2024-06-09 18:30:33 +0300

    Add new task: ImplementingStringMethod

---
+++ C# Fundamentals/18.StringsAndTextProcessing/06.ImplementingStringMethods/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _06.ImplementingStringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine(IndexOfS(text, "ice"));
+        }
+
+        static int IndexOfS(string str, string value) 
+        {
+            if (str == null || value == null)
+            {
+                Console.WriteLine($"Null value!!!!");
+            }
+
+            int strLen = str.Length;
+            int valueLen = str.Length;
+
+            if (valueLen == 0)
+            {
+                return 0;
+            }
+
+            if (valueLen > strLen)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i <= strLen - valueLen; i++)
+            {
+                int j = 0;
+                for (; j < valueLen; j++)
+                {
+                    if (str[i + j] != value[j])
+                    {
+                        break;
+                    }
+                }
+
+                if (j == valueLen)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
