bf233ed8cf90a95f5e41144cf28762f861918386
Konstantin Tinev <konstantintinev5@gmail.com>
2024-06-10 19:34:31 +0300

    Solved task 5

--- C# Fundamentals/19.ExerciseTextProcessing/05.MultiplyBigNumber/Program.cs
+++ C# Fundamentals/19.ExerciseTextProcessing/05.MultiplyBigNumber/Program.cs
@@ -1,28 +1,41 @@
-﻿namespace _05.MultiplyBigNumber
+﻿using System.Text;
+
+namespace _05.MultiplyBigNumber
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string firstNumber = Console.ReadLine();
-            int secondNumber = int.Parse(Console.ReadLine());
+            string bigNumber = Console.ReadLine();
+            int singleDigitChar = int.Parse(Console.ReadLine());
+            string result = MultiplyLargeNumber(bigNumber, singleDigitChar);
+            Console.WriteLine(result);
+        }
 
-            string newNumber = string.Empty;
-            int indexOfNewNumber = 0;
+        static string MultiplyLargeNumber(string bigNumber, int singleDigitChar)
+        {
+            if (singleDigitChar == '0' || bigNumber == "0")
+                return "0";
 
-            int currentNumber = 0;
-            for (int i = firstNumber.Length - 1; i >= 0 ; i++)
+            int singleDigit = singleDigitChar; // Превръщане на символа в число
+            int carry = 0;
+            StringBuilder result = new StringBuilder();
+
+            // Обработване на всяка цифра от края към началото
+            for (int i = bigNumber.Length - 1; i >= 0; i--)
             {
-                currentNumber = int.Parse(firstNumber[i].ToString());
-                currentNumber = currentNumber * secondNumber;
-
-                while (currentNumber != 0)
-                {
-
-                }
+                int digit = bigNumber[i] - '0'; // Превръщане на символа в число
+                int product = digit * singleDigit + carry;
+                result.Insert(0, product % 10); // Добавяне на последната цифра на произведението
+                carry = product / 10; // Обновяване на остатъка
             }
 
+            // Добавяне на останалия остатък ако има такъв
+            if (carry > 0)
+                result.Insert(0, carry);
 
+            return result.ToString();
         }
+
     }
 }
