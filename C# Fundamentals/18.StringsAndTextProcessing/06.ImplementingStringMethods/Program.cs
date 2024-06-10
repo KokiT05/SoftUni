ae88806e0ba7efbbe55c3895e9ecd8c8a0198c3e
Konstantin Tinev <konstantintinev5@gmail.com>
2024-06-10 10:42:03 +0300

    Add new topic Text processing exercise

--- C# Fundamentals/18.StringsAndTextProcessing/06.ImplementingStringMethods/Program.cs
+++ C# Fundamentals/18.StringsAndTextProcessing/06.ImplementingStringMethods/Program.cs
@@ -47,5 +47,51 @@
 
             return -1;
         }
+
+        static string RemoveS(string str, int startIndex, int count) 
+        {
+            if (str == null)
+            {
+                Console.WriteLine($"The string is Null!");
+            }
+
+            if (startIndex < 0 || startIndex >= str.Length)
+            {
+                Console.WriteLine($"The startIndex is out of range");
+            }
+
+            if (count < 0 || startIndex + count >= str.Length)
+            {
+                Console.WriteLine($"The count is incorrect!");
+            }
+
+            return str.Substring(0, startIndex) + str.Substring(startIndex + count);
+        }
+
+        static string SubStringS(string str, int startIndex, int length) 
+        {
+            if (str == null)
+            {
+                Console.WriteLine($"The string is Null!");
+            }
+
+            if (startIndex < 0 || startIndex >= str.Length)
+            {
+                Console.WriteLine($"The startIndex is out of range");
+            }
+
+            if (length < 0 || startIndex + length >= str.Length)
+            {
+                Console.WriteLine($"The length is incorrect!");
+            }
+
+            char[] result = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = str[startIndex + i];
+            }
+
+            return new string(result);
+        }
     }
 }
