f240bfbe38cbdfd51e227a2e1db6d6272986c16f
Konstantin Tinev <konstantintinev5@gmail.com>
2024-06-11 20:34:36 +0300

    Restore the topic Text Processing Exercise

---
+++ C# Fundamentals/19.TextProcessingExercise/05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05.MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
