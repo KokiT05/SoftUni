99f6e49ccc72e8c30902167cc46e4ad1d0a0dc8a
Konstantin Tinev <konstantintinev5@gmail.com>
2024-06-12 02:13:05 +0300

    a few added theme changes Associative Array Exercise

--- C# Fundamentals/17.AssociativeArraysExercise/04.Snowwhite/Program.cs
+++ C# Fundamentals/17.AssociativeArraysExercise/04.Snowwhite/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> drawfs = new Dictionary<string, int>();
 
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Once upon a time")
+            {
+                string[] inportArr = inputLine.Split(" <:> ");
+                string name = inportArr[0];
+                string hairColor = inportArr[1];
+                int physics = int.Parse(inportArr[2]);
+
+                string key = $"({hairColor}) {name}";
+
+                if (!drawfs.ContainsKey(key))
+                {
+                    drawfs.Add(key, 0);
+                }
+
+                if (drawfs[key] < physics)
+                {
+                    drawfs[key] = physics;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (var drawf in drawfs.OrderByDescending(kvp => kvp.Value)
+            .ThenByDescending
+            (c => drawfs.Where(kvp => kvp.Key.Split(")")[0] == c.Key.Split(")")[0]).Count()))
+            {
+                Console.WriteLine($"{drawf.Key} <-> {drawf.Value}");
+            }
         }
     }
 }
---
+++ C# Fundamentals/17.AssociativeArraysExercise/05.DragonArmy/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _05.DragonArmy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDragons = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, double[]>> dragonsInfo =
+                new Dictionary<string, Dictionary<string, double[]>>();
+
+            for (int i = 1; i <= numberOfDragons; i++)
+            {
+                string[] inputArr = Console.ReadLine().Split(' ');
+                string type = inputArr[0];
+                string name = inputArr[1];
+                double damage = 
+                inputArr[2] == "null" || inputArr[2] == "0" ? 45 : double.Parse(inputArr[2]);
+                double health =
+                inputArr[3] == "null" || inputArr[3] == "0" ? 250 : double.Parse(inputArr[3]);
+                double armor =
+                inputArr[4] == "null" || inputArr[4] == "0" ? 10 : double.Parse(inputArr[4]);
+
+                if (dragonsInfo.ContainsKey(type) == false)
+                {
+                    dragonsInfo.Add
+                    (type, new Dictionary<string, double[]> 
+                    { { name, new double[3] { damage, health, armor } } });
+                }
+                else if (dragonsInfo[type].ContainsKey(name) == false)
+                {
+                    dragonsInfo[type].Add(name, new double[3] { damage, health, armor });
+                }
+                else
+                {
+                    double[] newStats = new double[3] {damage, health, armor};
+                    dragonsInfo[type][name] = newStats;
+                }
+            }
+
+            foreach (string type in dragonsInfo.Keys)
+            {
+                Console.Write($"{type}::");
+                Console.Write($"({dragonsInfo[type].Values.Average(v => v[0]):f2}/");
+                Console.Write($"{dragonsInfo[type].Values.Average(v => v[1]):f2}/");
+                Console.Write($"{dragonsInfo[type].Values.Average(v => v[2]):f2})\n");
+
+                foreach (KeyValuePair<string, double[]> dragon in 
+                    dragonsInfo[type].OrderBy(k => k.Key))
+                {
+                    Console.Write($"-{dragon.Key} -> ");
+                    Console.Write($"damage: { dragon.Value[0]}, ");
+                    Console.Write($"health: {dragon.Value[1]}, ");
+                    Console.Write($"armor: {dragon.Value[2]}\n");
+                }
+            }
+        }
+    }
+}
