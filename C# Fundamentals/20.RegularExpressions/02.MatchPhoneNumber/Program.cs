6d45d41fe73705f2846d98801556909bf8b26f92
Konstantin Tinev <konstantintinev5@gmail.com>
2024-06-14 05:02:39 +0300

    Add new topic: RegularExpressions

---
+++ C# Fundamentals/20.RegularExpressions/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\+359)(\s|\-)2(\s|\-)[0-9]{3}(\s|\-)[0-9]{4}";
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matchCollection = regex.Matches(input);
+            
+            string[] allPhoneNumber = matchCollection.Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", allPhoneNumber));
+        }
+    }
+}
