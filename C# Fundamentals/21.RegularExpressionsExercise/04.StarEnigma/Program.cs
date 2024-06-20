8c09bebeb918f1c3dd42275a50f83e3d65eb2954
Konstantin Tinev <konstantintinev5@gmail.com>
2024-06-20 13:25:37 +0300

    Add new tasks from the topic Regular Expressions Exercise

---
+++ C# Fundamentals/21.RegularExpressionsExercise/04.StarEnigma/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+
+namespace _04.StarEnigma
+{
+    class Planet
+    {
+        public Planet(string name, int population, string attackType, int soldierCount)
+        {
+            this.Name = name;
+            this.Population = population;
+            this.AttackType = attackType;
+            this.SoldierCount = soldierCount;
+        }
+
+        public string Name { get; set; }
+
+        public int Population { get; set; }
+
+        public string AttackType { get; set; }
+
+        public int SoldierCount { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfLines = int.Parse(Console.ReadLine());
+
+            List<Planet> planets = new List<Planet>();
+
+            string pattern = @"[starSTAR]";
+            Regex regex = new Regex(pattern);
+
+            StringBuilder decryptMessage = new StringBuilder();
+            string planeName = string.Empty;
+            int population = 0;
+            string attackType = string.Empty;
+            int soldierCount = 0;
+
+            for (int i = 1; i <= countOfLines; i++)
+            {
+                string inputMessage = Console.ReadLine();
+                decryptMessage.Clear();
+                pattern = @"[starSTAR]";
+                regex = new Regex(pattern);
+
+                MatchCollection match = regex.Matches(inputMessage);
+                int countOfMatches = match.Count;
+
+                foreach (char character in inputMessage)
+                {
+                    char decryptChar = (char)(character - countOfMatches);
+                    decryptMessage.Append(decryptChar);
+                }
+
+                pattern =
+            @"@(?<planeName>[A-Za-z]+)\w?:(?<population>[0-9]+)!(?<attackType>[A-Za-z])!->(?<soldier>[0-9]+)";
+                regex = new Regex(pattern);
+                Match decryptMatch = regex.Match(decryptMessage.ToString());
+
+                if (decryptMatch.Success)
+                {
+                    planeName = decryptMatch.Groups["planeName"].Value;
+                    population = int.Parse(decryptMatch.Groups["population"].Value);
+                    attackType = decryptMatch.Groups["attackType"].Value;
+                    soldierCount = int.Parse(decryptMatch.Groups["soldier"].Value);
+
+                    Planet planet = new Planet(planeName, population, attackType, soldierCount);
+                    planets.Add(planet);
+                }
+            }
+
+            int countofAttackType = planets.Where(attackType => attackType.AttackType == "A").Count();
+
+            Console.WriteLine($"Attacked planets: {countofAttackType}");
+            foreach (Planet planet in planets.Where(attackType => attackType.AttackType == "A"))
+            {
+                Console.WriteLine($"-> {planet.Name}");
+            }
+
+            countofAttackType = planets.Where(attackType => attackType.AttackType == "D").Count();
+
+            Console.WriteLine($"Destroyed planets: {countofAttackType}");
+            foreach (Planet planet in planets.Where(attackType => attackType.AttackType == "D"))
+            {
+                Console.WriteLine($"-> {planet.Name}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/21.RegularExpressionsExercise/05.NetherRealms/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _05.NetherRealms
+{
+    class Demon
+    {
+        public Demon(string name, int health, double damage)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Damage = damage;
+        }
+
+        public string Name { get; set; }
+
+        public int Health { get; set; }
+
+        public double Damage { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Health} health, {Damage:f2} damage";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Demon> allDemons = new List<Demon>();
+
+            string pattern = @"[-+]?[0-9]+\.?[0-9]*";
+
+            Regex regex = new Regex(pattern);
+
+            string[] input = Console.ReadLine().Split(new[] { ',', ' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string demon in input) 
+            {
+                string filter = "0123456789+-*/.";
+
+                int health = demon.Where(ch => !filter.Contains(ch)).Sum(ch => ch);
+                double damage = CalculateDamage(regex, demon);
+                string name = demon;
+                Demon currentDemon = new Demon(name, health, damage);
+                allDemons.Add(currentDemon);
+            }
+
+            allDemons = allDemons.OrderBy(d => d.Name).ToList();
+            foreach (Demon demon in allDemons)
+            {
+                Console.WriteLine(demon);
+            }
+        }
+
+        public static double CalculateDamage(Regex regex, string demon)
+        {
+            MatchCollection matchCollection = regex.Matches(demon);
+
+            double damage = 0;
+            foreach (Match match in matchCollection)
+            {
+                damage += double.Parse(match.Value);
+            }
+
+            foreach (char oneChar in demon)
+            {
+                if (oneChar == '*')
+                {
+                    damage = damage * 2.0;
+                }
+                else if (oneChar == '/')
+                {
+                    damage = damage / 2.0;
+                }
+            }
+
+            return damage;
+        }
+    }
+}
