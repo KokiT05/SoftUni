3f8c8a80fa69f681d9e9c03abd09c85d30a30bf0
Konstantin Tinev <konstantintinev5@gmail.com>
2024-07-10 16:45:06 +0300

    Last task from the topic Stacks and Queues

---
+++ C# Advanced/01.StacksAndQueues/08.TrafficJam/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _08.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> cars = new Queue<string>();
+            int sumOfPassedCars = 0;
+
+            int countPass = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            while (input != "end") 
+            {
+                if (input == "green")
+                {
+                    sumOfPassedCars = sumOfPassedCars + countPass;
+
+                    for (int i = 1; i <= countPass; i++)
+                    {
+                        if (cars.Count == 0)
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                    }
+                }
+
+                cars.Enqueue(input);
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{sumOfPassedCars} cars passed the crossroads.");
+        }
+    }
+}
