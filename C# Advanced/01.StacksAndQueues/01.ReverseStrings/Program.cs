6d3dc678f95f829432007d44324791d76951e9ef
Konstantin Tinev <konstantintinev5@gmail.com>
2024-07-10 13:52:46 +0300

    Add new course C# Advanced

---
+++ C# Advanced/01.StacksAndQueues/01.ReverseStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string inputText = Console.ReadLine();
+            //Stack<char> stack = new Stack<char>(inputText);
+
+            //foreach (char c in stack)
+            //{
+            //    Console.Write(c);
+            //}
+
+            string inputText = Console.ReadLine();
+
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                stack.Push(inputText[i].ToString());
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
