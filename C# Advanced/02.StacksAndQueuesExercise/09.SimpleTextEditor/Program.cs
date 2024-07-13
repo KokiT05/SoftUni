5b19ff0d1b754a16b99b3d2bb05c4cdbd34d85b1
Konstantin Tinev <konstantintinev5@gmail.com>
2024-07-13 13:48:08 +0300

    Add new tasks from the topic Stacks and Queues Exercise

--- C# Advanced/02.StacksAndQueuesExercise/09.SimpleTextEditor/Program.cs
+++ C# Advanced/02.StacksAndQueuesExercise/09.SimpleTextEditor/Program.cs
@@ -6,58 +6,92 @@ namespace _09.SimpleTextEditor
     {
         static void Main(string[] args)
         {
-            Stack<string> textStack = new Stack<string>();
-            //string currentText = string.Empty;
+            int n = int.Parse(Console.ReadLine());
 
-            StringBuilder currentText = new StringBuilder();
-            int nLines = int.Parse(Console.ReadLine());
-            for (int i = 0; i < nLines; i++)
+            var builder = new StringBuilder();
+            var stack = new Stack<string>();
+            stack.Push(builder.ToString());
+
+            for (int i = 0; i < n; i++)
             {
-                string[] inputData = Console.ReadLine().Split();
-                string command = inputData[0];
-                if (command == "1")
-                {
-                    string subString = inputData[1];
-                    currentText.Append(subString);
-                    textStack.Push(currentText.ToString());
-                }
-                else if (command == "2")
-                {
-                    string subString = inputData[1];
-                    int count = int.Parse(subString);
+                string[] input = Console.ReadLine().Split();
+                int command = int.Parse(input[0]);
 
-                    if (count >= 0 && count <= currentText.Length)
-                    {
-
-                        currentText.Remove(currentText.Length - count, count);
-                        textStack.Push(currentText.ToString());
-                    }
-                }
-                else if (command == "3")
+                switch (command)
                 {
-                    string subString = inputData[1];
-                    int index = int.Parse(subString) - 1;
-
-                    string symbol = textStack.Peek();
-
-                    if (index >= 0 && index <= symbol.Length)
-                    {
-                        Console.WriteLine(symbol[index]);
-                    }
-                }
-                else if (command == "4")
-                {
-                    currentText.Clear();
-                    if (textStack.Count > 1)
-                    {
-                        currentText.Append(textStack.Pop());
-                    }
-                    else
-                    {
-                        textStack.Pop();
-                    }
+                    case 1:
+                        builder.Append(input[1]);
+                        stack.Push(builder.ToString());
+                        break;
+                    case 2:
+                        int number = int.Parse(input[1]);
+                        builder.Remove(builder.Length - number, number);
+                        stack.Push(builder.ToString());
+                        break;
+                    case 3:
+                        int index = int.Parse(input[1]);
+                        Console.WriteLine(builder[index - 1]);
+                        break;
+                    case 4:
+                        stack.Pop();
+                        builder = new StringBuilder();
+                        builder.Append(stack.Peek());
+                        break;
                 }
             }
+            //Stack<string> textStack = new Stack<string>();
+            ////string currentText = string.Empty;
+
+            //StringBuilder currentText = new StringBuilder();
+            //int nLines = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < nLines; i++)
+            //{
+            //    string[] inputData = Console.ReadLine().Split();
+            //    string command = inputData[0];
+            //    if (command == "1")
+            //    {
+            //        string subString = inputData[1];
+            //        currentText.Append(subString);
+            //        textStack.Push(currentText.ToString());
+            //    }
+            //    else if (command == "2")
+            //    {
+            //        string subString = inputData[1];
+            //        int count = int.Parse(subString);
+
+            //        if (count >= 0 && count <= currentText.Length)
+            //        {
+
+            //            currentText.Remove(currentText.Length - count, count);
+            //            textStack.Push(currentText.ToString());
+            //        }
+            //    }
+            //    else if (command == "3")
+            //    {
+            //        string subString = inputData[1];
+            //        int index = int.Parse(subString) - 1;
+
+            //        string symbol = string.Empty;
+            //        if (textStack.Count > 0)
+            //        {
+            //            symbol = textStack.Peek();
+            //        }
+
+            //        if (index >= 0 && index <= symbol.Length)
+            //        {
+            //            Console.WriteLine(symbol[index]);
+            //        }
+            //    }
+            //    else if (command == "4")
+            //    {
+            //        currentText.Clear();
+            //        if (textStack.Count > 0)
+            //        {
+            //            textStack.Pop();
+            //            currentText.Append(textStack.Peek());
+            //        }
+            //    }
+            //}
         }
     }
 }
---
+++ C# Advanced/02.StacksAndQueuesExercise/10.Crossroads/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _10.Crossroads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int durationOfGreenLight = int.Parse(Console.ReadLine());
+            int durationOfFreeWindow = int.Parse(Console.ReadLine());
+
+            Queue<char> carCharQueue = new Queue<char>();
+            int countCarPass = 0;
+            int timeToPass = durationOfGreenLight;
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                if (command == "green")
+                {
+                    if (durationOfFreeWindow < carCharQueue.Count)
+                    {
+                        for (int i = 0; i < durationOfFreeWindow; i++)
+                        {
+                            carCharQueue.Dequeue();
+                        }
+                        Console.WriteLine($"{carCharQueue.Peek()} A crash happened!");
+                        break;
+                    }
+
+                    if ()
+                    {
+
+                    }
+                }
+                else if (command != "green")
+                {
+                    carCharQueue = new Queue<char>(command.ToCharArray());
+                }
+            }
+        }
+    }
+}
