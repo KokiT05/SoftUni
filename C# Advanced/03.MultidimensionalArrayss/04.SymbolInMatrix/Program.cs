2f40b296a268d64118f053f89c3a6f34ef12c26f
Konstantin Tinev <konstantintinev5@gmail.com>
2024-07-24 23:41:31 +0300

    Add new task

---
+++ C# Advanced/03.MultidimensionalArrayss/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _04.SymbolInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[,] matrix = new string[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string input = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col].ToString();
+                }
+            }
+
+            string symbol = Console.ReadLine();
+            bool isFind = false;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (symbol == matrix[row, col].ToString())
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isFind = true;
+                        break;
+                    }
+                }
+
+                if (isFind)
+                {
+                    break;
+                }
+            }
+
+            if (!isFind)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
