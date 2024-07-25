fe723014fba01836b6b6abff6ef8759bd0e536f8
Konstantin Tinev <konstantintinev5@gmail.com>
2024-07-25 17:01:42 +0300

    Add new tasks

---
+++ C# Advanced/03.MultidimensionalArrayss/05.SquareWithMaximumSum/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _05.SquareWithMaximumSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixData = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rowMatrix = matrixData[0];
+            int colMatrix = matrixData[1];
+            int[,] matrix = new int[rowMatrix, colMatrix];
+
+            for (int row = 0; row < matrix.GetLength(0); row++) 
+            {
+                int[] inputData = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputData[col];
+                }
+            }
+
+            int maxValueSuqearSum = int.MinValue;
+            int sumOfSquear = 0;
+            int rowIndex = 0;
+            int colIndex = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    sumOfSquear = matrix[row, col] + 
+                                matrix[row, col + 1] + 
+                                matrix[row + 1, col] +
+                                matrix[row + 1, col + 1];
+
+                    if (maxValueSuqearSum < sumOfSquear)
+                    {
+                        rowIndex = row;
+                        colIndex = col;
+                        maxValueSuqearSum = sumOfSquear;
+                    }
+                }
+            }
+
+            for (int row = rowIndex; row < rowIndex + 2; row++)
+            {
+                for (int col = colIndex; col < colIndex + 2; col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxValueSuqearSum);
+        }
+    }
+}
---
+++ C# Advanced/03.MultidimensionalArrayss/06.Jagged-ArrayModification/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _06.Jagged_ArrayModification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[][] jaggedArray = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] inputData = Console.ReadLine()
+                                    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+                jaggedArray[row] = new int[inputData.Length];
+
+                for (int col = 0; col < inputData.Length; col++)
+                {
+                    jaggedArray[row][col] = inputData[col];
+                }
+            }
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] commandArg = command.Split();
+                string action = commandArg[0];
+                int coordinatesRow = int.Parse(commandArg[1]);
+                int coordinatesCol = int.Parse(commandArg[2]);
+                int value = int.Parse(commandArg[3]);
+
+                if (action == "Add" && 
+                    ISValidCoordinates(coordinatesRow, coordinatesCol, jaggedArray))
+                {
+                    jaggedArray[coordinatesRow][coordinatesCol] += value;
+                }
+                else if (action == "Subtract" && 
+                    ISValidCoordinates(coordinatesRow, coordinatesCol, jaggedArray))
+                {
+                    jaggedArray[coordinatesRow][coordinatesCol] -= value;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (int[] array in jaggedArray)
+            {
+                Console.WriteLine(string.Join(' ', array));
+            }
+        }
+
+        static bool ISValidCoordinates(int row, int col, int[][] jaggedArray)
+        {
+            if (row < 0 || row >= jaggedArray.Length || col < 0 || col >= jaggedArray[row].Length)
+            {
+                Console.WriteLine("Invalid coordinates");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
