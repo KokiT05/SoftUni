b80cebd82910f370f7a5d7e5abcc9c8638bcae41
Konstantin Tinev <konstantintinev5@gmail.com>
2024-07-25 19:46:11 +0300

    Add new task and bonus

--- C# Advanced/03.MultidimensionalArrayss/07.PascalTriangle/Program.cs
+++ C# Advanced/03.MultidimensionalArrayss/07.PascalTriangle/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int rows = int.Parse(Console.ReadLine());
+
+            long[][] jaggedArray = new long[rows][];
+
+            long currentValue = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                jaggedArray[row] = new long[row + 1];
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    if (row - 1 >= 0 && 
+                        jaggedArray[row - 1].Length > col && col - 1 >= 0)
+                    {
+                        currentValue = jaggedArray[row - 1][col];
+                        currentValue += jaggedArray[row - 1][col - 1];
+                        jaggedArray[row][col] = currentValue;
+                    }
+                    else
+                    {
+                        jaggedArray[row][col] = 1;
+                    }
+                }
+            }
+
+            int count = 0;
+            foreach (long[] array in jaggedArray)
+            {
+                Console.WriteLine($"{new string(' ', rows - count)}{string.Join(" ", array)}");
+                count++;
+            }
         }
     }
 }
---
+++ C# Advanced/03.MultidimensionalArrayss/08.SpiralMatrix/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace _08.SpiralMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            string direction = "left";
+
+
+            int row = 0;
+            int col = 0;
+            for (int i = 1; i <= n * n; i++)
+            {
+                if (direction == "left")
+                {
+                    matrix[row, col] = i;
+                    col++;
+                }
+                else if (direction == "down")
+                {
+                    matrix[row, col] = i;
+                    row++;
+                }
+                else if (direction == "right")
+                {
+                    matrix[row, col] = i;
+                    col--;
+                }
+                else if (direction == "up")
+                {
+                    matrix[row, col] = i;
+                    row--;
+                }
+
+                if (direction == "left" && (col >= n || matrix[row, col] != 0))
+                {
+                    col--;
+                    row++;
+                    direction = "down";
+                }
+                else if (direction == "down" && (row >= n || matrix[row, col] != 0))
+                {
+                    row--;
+                    col--;
+                    direction = "right";
+                }
+                else if (direction == "right" && (col < 0 || matrix[row, col] != 0))
+                {
+                    col++;
+                    row--;
+                    direction = "up";
+                }
+                else if (direction == "up" && (row < 0 || matrix[row, col] != 0))
+                {
+                    row++;
+                    col++;
+                    direction = "left";
+                }
+
+            }
+            Console.WriteLine();
+
+            for (int rowMatrix = 0; rowMatrix < matrix.GetLength(0); rowMatrix++)
+            {
+                for (int colMatrix = 0; colMatrix < matrix.GetLength(1); colMatrix++)
+                {
+                    if (matrix[rowMatrix, colMatrix] < 10)
+                    {
+                        Console.Write($" {matrix[rowMatrix, colMatrix]} ");
+                    }
+                    else
+                    {
+                        Console.Write($"{matrix[rowMatrix, colMatrix]} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
