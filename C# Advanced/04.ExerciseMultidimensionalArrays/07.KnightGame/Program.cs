9887a137a50bc170ded0cc4b9e5482bb75319ae8
Konstantin Tinev <konstantintinev5@gmail.com>
2024-08-03 18:24:20 +0300

    rewritten task 7 Knight Game

--- C# Advanced/04.ExerciseMultidimensionalArrays/07.KnightGame/Program.cs
+++ C# Advanced/04.ExerciseMultidimensionalArrays/07.KnightGame/Program.cs
@@ -1,252 +1,127 @@
-﻿namespace _07.KnightGame
+﻿
+namespace _07.KnightGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int boardSide = int.Parse(Console.ReadLine());
 
-            string[,] board = new string[boardSide, boardSide];
+            int n = int.Parse(Console.ReadLine());
+            char[,] board = new char[n, n];
 
-            // Filling the board
-            for (int row = 0; row < board.GetLength(0); row++)
+            for (int row = 0; row < n; row++)
             {
-                string currentRow = Console.ReadLine();
-                for (int col = 0; col < board.GetLength(1); col++)
+                char[] rowData = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < n; col++)
                 {
-                    board[row, col] = currentRow[col].ToString();
+                    board[row, col] = rowData[col];
                 }
             }
 
-            int knightsInDangerCounter = 0; // Counting how many knights can be attacked from current knight.
-            int removedKnightsCounter = 0; // Counting removed knights from the board.
+            int removedKnights = 0;
 
-            for (int maxAttackPotential = 8; maxAttackPotential > 0; maxAttackPotential--) // Every knight can attack and kill maximum 8 other knights. P.S. Check "Possible moves" at the beginning.
+            while (true)
             {
-                for (int row = 0; row < board.GetLength(0); row++)
+                int maxAttackedKnightsCount = 0;
+                int knightRow = 1;
+                int knightCol = 1;
+
+                for (int row = 0; row < n; row++)
                 {
-                    for (int col = 0; col < board.GetLength(1); col++)
+                    for (int col = 0; col < n; col++)
                     {
-                        if (board[row, col].ToLower() == "k") // Check if we have knight on current position.
+                        char symbol = board[row, col];
+
+                        if (symbol != 'K')
                         {
-                            // Check if new position isn't outside of the board (so we don't get IndexOutOfRangeException).
-                            if (row - 1 >= 0)
-                            {
-                                if (col - 2 >= 0)
-                                {
-                                    // Check if there is knight to be attacked on the new possition.
-                                    if (board[row - 1, col - 2].ToLower() == "k")
-                                    {
-                                        knightsInDangerCounter++;
-                                    }
-                                }
-
-                                if (col + 2 < board.GetLength(1))
-                                {
-                                    // Check if there is knight to be attacked on the new possition.
-                                    if (board[row - 1, col + 2].ToLower() == "k")
-                                    {
-                                        knightsInDangerCounter++;
-                                    }
-                                }
-                            }
-
-                            if (row + 1 < board.GetLength(0))
-                            {
-                                if (col - 2 >= 0)
-                                {
-                                    // Check if there is knight to be attacked on the new possition
-                                    if (board[row + 1, col - 2].ToLower() == "k")
-                                    {
-                                        knightsInDangerCounter++;
-                                    }
-                                }
-
-                                if (col + 2 < board.GetLength(1))
-                                {
-                                    // Check if there is knight to be attacked on the new possition
-                                    if (board[row + 1, col + 2].ToLower() == "k")
-                                    {
-                                        knightsInDangerCounter++;
-                                    }
-                                }
-                            }
-
-                            if (row - 2 >= 0)
-                            {
-                                if (col - 1 >= 0)
-                                {
-                                    // Check if there is knight to be attacked on the new possition
-                                    if (board[row - 2, col - 1].ToLower() == "k")
-                                    {
-                                        knightsInDangerCounter++;
-                                    }
-                                }
-
-                                if (col + 1 < board.GetLength(1))
-                                {
-                                    // Check if there is knight to be attacked on the new possition
-                                    if (board[row - 2, col + 1].ToLower() == "k")
-                                    {
-                                        knightsInDangerCounter++;
-                                    }
-                                }
-                            }
-
-                            if (row + 2 < board.GetLength(0))
-                            {
-                                if (col - 1 >= 0)
-                                {
-                                    // Check if there is knight to be attacked on the new possition
-                                    if (board[row + 2, col - 1].ToLower() == "k")
-                                    {
-                                        knightsInDangerCounter++;
-                                    }
-                                }
-
-                                if (col + 1 < board.GetLength(1))
-                                {
-                                    // Check if there is knight to be attacked on the new possition
-                                    if (board[row + 2, col + 1].ToLower() == "k")
-                                    {
-                                        knightsInDangerCounter++;
-                                    }
-                                }
-                            }
+                            continue;
                         }
 
-                        // Check if every possible attack is against another knight (attack potential of current knight is biggest possible)
-                        if (knightsInDangerCounter == maxAttackPotential)
-                        {
-                            board[row, col] = "0"; // Remove knight from board.
-                            removedKnightsCounter++;
-                        }
+                        int count = GetCountOfAttackedKnights(board, row, col);
 
-                        knightsInDangerCounter = 0; // Restart counter.
+                        if (count > maxAttackedKnightsCount)
+                        {
+                            maxAttackedKnightsCount = count;
+                            knightRow = row;
+                            knightCol = col;
+                        }
                     }
                 }
+
+                if (maxAttackedKnightsCount == 0)
+                {
+                    break;
+                }
+
+                board[knightRow, knightCol] = '0';
+                removedKnights++;
             }
 
-            // Print result
-            Console.WriteLine(removedKnightsCounter);
-
-            //int n = int.Parse(Console.ReadLine());
-
-            //char[,] matrix = new char[n, n];
-
-            //Dictionary<string, int> knightsAttack = new Dictionary<string, int>();
-            //int countOfKnight = 0;
-
-            //for (int row = 0; row < n; row++)
-            //{
-            //    string inputRowData = Console.ReadLine();
-
-            //    for (int col = 0; col < n; col++)
-            //    {
-            //        matrix[row, col] = inputRowData[col];
-            //        if (inputRowData[col] == 'K')
-            //        {
-            //            countOfKnight++;
-            //            knightsAttack.Add($"{row}{col}", 0);
-            //        }
-            //    }
-            //}
-
-            //int knightAttackMax = int.MaxValue;
-            //int countOfIteration = 0;
-            //while (knightAttackMax > 0)
-            //{
-            //    for (int row = 0; row < n; row++)
-            //    {
-            //        for (int col = 0; col < n; col++)
-            //        {
-            //            if (matrix[row, col] == 'K' && knightsAttack.ContainsKey($"{row}{col}"))
-            //            {
-            //                if ((row + 1 < n && col + 2 < n) &&
-            //                    matrix[row + 1, col + 2] == 'K')
-            //                {
-            //                    knightsAttack[$"{row}{col}"]++;
-            //                }
-
-            //                if ((row - 1 >= 0 && col + 2 < n) &&
-            //                    matrix[row - 1, col + 2] == 'K')
-            //                {
-            //                    knightsAttack[$"{row}{col}"]++;
-            //                }
-
-            //                if (
-            //                    (row + 2 < n && col + 1 < n) &&
-            //                    matrix[row + 2, col + 1] == 'K')
-            //                {
-            //                    knightsAttack[$"{row}{col}"]++;
-            //                }
-
-            //                if ((row + 2 < n && col - 1 >= 0) &&
-            //                    matrix[row + 2, col - 1] == 'K')
-            //                {
-            //                    knightsAttack[$"{row}{col}"]++;
-            //                }
-
-            //                if (
-            //                    (row + 1 < n && col - 2 >= 0) &&
-            //                    matrix[row + 1, col - 2] == 'K')
-            //                {
-            //                    knightsAttack[$"{row}{col}"]++;
-            //                }
-
-            //                if ((row - 1 >= 0 && col - 2 >= 0) &&
-            //                    matrix[row - 1, col - 2] == 'K')
-            //                {
-            //                    knightsAttack[$"{row}{col}"]++;
-            //                }
-
-            //                if ((row - 2 >= 0 && col + 1 < n) &&
-            //                    matrix[row - 2, col + 1] == 'K')
-            //                {
-            //                    knightsAttack[$"{row}{col}"]++;
-            //                }
+            Console.WriteLine(removedKnights);
 
 
-            //                if ((row - 2 >= 0 && col - 1 >= 0) &&
-            //                    matrix[row - 2, col - 1] == 'K')
-            //                {
-            //                    knightsAttack[$"{row}{col}"]++;
-            //                }
-            //            }
-            //        }
-            //    }
+        }
 
-            //    if (knightsAttack.Count > 0)
-            //    {
-            //        knightAttackMax = knightsAttack.Max(v => v.Value);
-            //    }
-            //    if (knightAttackMax > 0 && knightsAttack.Count > 0)
-            //    {
-            //        string coordinates = knightsAttack.First(v => (v.Value == knightAttackMax)).Key;
-            //        int rowCoordinates = int.Parse(coordinates[0].ToString());
-            //        int colCoordinates = int.Parse(coordinates[1].ToString());
-            //        matrix[rowCoordinates, colCoordinates] = '0';
-            //        knightsAttack.Remove(coordinates);
+        private static int GetCountOfAttackedKnights(char[,] board, int row, int col)
+        {
+            int count = 0;
 
-            //        foreach (string coordinatesKey in knightsAttack.Keys)
-            //        {
-            //            knightsAttack[coordinatesKey] = 0;
-            //        }
-            //    }
-            //    else
-            //    {
-            //        break;
-            //    }
-            //    countOfIteration++;
-            //}
+            if (ContainsKnight(board, row - 2, col - 1))
+            {
+                count++;
+            }
 
+            if (ContainsKnight(board, row - 2, col + 1))
+            {
+                count++;
+            }
 
-            //Console.WriteLine(countOfIteration);
+            if (ContainsKnight(board, row - 1, col - 2))
+            {
+                count++;
+            }
 
+            if (ContainsKnight(board, row - 1, col + 2))
+            {
+                count++;
+            }
 
-            //knightsAttack = knightsAttack
-            //    .OrderByDescending(v => v.Value)
-            //    .ToDictionary(k => k.Key, v => v.Value);
+            if (ContainsKnight(board, row + 1, col - 2))
+            {
+                count++;
+            }
+
+            if (ContainsKnight(board, row + 1, col + 2))
+            {
+                count++;
+            }
+
+            if (ContainsKnight(board, row + 2, col - 1))
+            {
+                count++;
+            }
+
+            if (ContainsKnight(board, row + 2, col + 1))
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        private static bool ContainsKnight(char[,] board, int row, int col)
+        {
+            if (!IsValidCell(row, col, board.GetLength(0)))
+            {
+                return false;
+            }
+
+            return board[row, col] == 'K';
+        }
+
+        private static bool IsValidCell(int row, int col, int lenght)
+        {
+            return row >= 0 && row < lenght && col >= 0 && col < lenght;
         }
     }
 }
--- C# Advanced/04.ExerciseMultidimensionalArrays/10.RadioactiveMutantVampireBunnies/Program.cs
+++ C# Advanced/04.ExerciseMultidimensionalArrays/10.RadioactiveMutantVampireBunnies/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+
 namespace _10.RadioactiveMutantVampireBunnies
 {
     internal class Program
@@ -59,9 +60,109 @@ namespace _10.RadioactiveMutantVampireBunnies
                 {
                     isWon = true;
                     matrix[playerRow, playerCol] = '.';
+                    List<int[]> bunniesCoordinates = GetBunniesCoordinates(matrix);
+                    SpreadBunnies(bunniesCoordinates, matrix);
+                }
+                else if (matrix[newPlayerRow, newPlayerCol] == '.')
+                {
+                    matrix[playerRow, playerCol] = '.';
+                    matrix[newPlayerRow, newPlayerCol] = 'P';
+                    playerRow = newPlayerRow;
+                    playerCol = newPlayerCol;
+                    List<int[]> bunniesCoordinates = GetBunniesCoordinates(matrix);
+                    SpreadBunnies(bunniesCoordinates, matrix);
+
+                    if (matrix[playerRow, playerCol] == 'B')
+                    {
+                        isDead = true;
+                    }
+                }
+                else if (matrix[newPlayerRow, newPlayerCol] == 'B')
+                {
+                    isDead = true;
+                    matrix[playerRow, playerCol] = '.';
+                    playerRow = newPlayerRow;
+                    playerCol = newPlayerCol;
+                    List<int[]> bunniesCoordinates = GetBunniesCoordinates(matrix);
+                    SpreadBunnies(bunniesCoordinates, matrix);
+                }
+
+                if (isDead || isWon)
+                {
+                    break;
                 }
             }
 
+            PrintField(matrix);
+            string result = string.Empty;
+
+            if (isWon)
+            {
+                result += "won";
+            }
+
+            if (isDead)
+            {
+                result += "dead";
+            }
+
+            result += $": {playerRow} {playerCol}";
+            Console.WriteLine(result);
+        }
+
+        private static void PrintField(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void SpreadBunnies(List<int[]> bunniesCoordinates, char[,] matrix)
+        {
+            foreach (int[] bunnyCoordinates in bunniesCoordinates)
+            {
+                int row = bunnyCoordinates[0];
+                int col = bunnyCoordinates[1];
+
+                SpreadBunny(row - 1, col, matrix);
+                SpreadBunny(row + 1, col, matrix);
+                SpreadBunny(row, col - 1, matrix);
+                SpreadBunny(row, col + 1, matrix);
+            }
+        }
+
+        private static void SpreadBunny(int row, int col, char[,] matrix)
+        {
+            int rowsLenght = matrix.GetLength(0);
+            int colsLenght = matrix.GetLength(1);
+
+            if (IsValidCell(row, col, rowsLenght, colsLenght))
+            {
+                matrix[row, col] = 'B';
+            }
+        }
+
+        private static List<int[]> GetBunniesCoordinates(char[,] matrix)
+        {
+            List<int[]> bunniesCoordinates = new List<int[]>();
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 'B')
+                    {
+                        bunniesCoordinates.Add(new int[] { row, col });
+                    }
+                }
+            }
+
+            return bunniesCoordinates;
         }
 
         private static bool IsValidCell(int row, int col, int rowSize, int colSize)
