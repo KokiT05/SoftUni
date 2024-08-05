aadeea4d924634d5ba7729513881430d687463f3
Konstantin Tinev <konstantintinev5@gmail.com>
2024-08-05 16:13:12 +0300

    Last tasks from the topic Sets and Dictionaries Advanced

---
+++ C# Advanced/05.SetsAndDictionariesAdvanced/07.ParkingLot/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            HashSet<string> cars = new HashSet<string>();
+            while (command != "END")
+            {
+                string[] commandArg = command.Split(", ");
+                string direction = commandArg[0];
+                string carNumber = commandArg[1];
+
+                if (direction == "IN")
+                {
+                    cars.Add(carNumber);
+                }
+                else if (direction == "OUT")
+                {
+                    cars.Remove(carNumber);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (cars.Count > 0)
+            {
+                foreach (string car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ C# Advanced/05.SetsAndDictionariesAdvanced/08.SoftUniParty/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _08.SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string reservation = Console.ReadLine();
+
+            HashSet<string> vipReservations = new HashSet<string>();
+            HashSet<string> regularReservations = new HashSet<string>();
+            while (reservation != "PARTY")
+            {
+                if (char.IsDigit(reservation[0]))
+                {
+                    vipReservations.Add(reservation);
+                }
+                else
+                {
+                    regularReservations.Add(reservation);
+                }
+
+                reservation = Console.ReadLine();
+            }
+
+            reservation = Console.ReadLine();
+            while (reservation != "END" && (vipReservations.Count > 0 || regularReservations.Count > 0))
+            {
+                if (vipReservations.Contains(reservation))
+                {
+                    vipReservations.Remove(reservation);
+                }
+                else if (regularReservations.Contains(reservation))
+                {
+                    regularReservations.Remove(reservation);
+                }
+
+                reservation = Console.ReadLine();
+            }
+
+            Console.WriteLine(vipReservations.Count + regularReservations.Count);
+            foreach (string vipReservation in vipReservations)
+            {
+                Console.WriteLine(vipReservation);
+            }
+
+            foreach (string regularReservation in regularReservations)
+            {
+                Console.WriteLine(regularReservation);
+            }
+        }
+    }
+}
