4849e4a829ce7409d6c8dfa1f4e47b95c4d18e5d
Konstantin Tinev <konstantintinev5@gmail.com>
2024-08-19 13:03:23 +0300

    add new tasks

---
+++ C# Advanced/07.StreamsFilesAndDirectories/03.WordCount/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace _03.WordCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+            string text = string.Empty;
+            string[] words;
+            using (StreamReader streamReader = new StreamReader("../../../Words.txt"))
+            {
+                text = streamReader.ReadToEnd();
+                while (text != null)
+                {
+                    words = text.Split(" ");
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        if (!wordsCount.ContainsKey(words[i]))
+                        {
+                            wordsCount.Add(words[i], 0);
+                        }
+                    }
+                    text = streamReader.ReadLine();
+                }
+            }
+
+            using (StreamReader streamReader = new StreamReader("../../../Input.txt"))
+            {
+                text = streamReader.ReadLine();
+                while (text != null)
+                {
+                    words = text.Split(new char[] { ' ', '-', ',', '.', '!', '?'})
+                        .Select(word => word.ToLower())
+                        .ToArray();
+                    foreach (string word in words)
+                    {
+                        if (wordsCount.ContainsKey(word))
+                        {
+                            wordsCount[word]++;
+                        }
+                    }
+                    text = streamReader.ReadLine();
+                }
+            }
+
+            wordsCount = wordsCount.OrderByDescending(x => x.Value)
+                                    .ToDictionary(key => key.Key, value => value.Value);
+
+            using (StreamWriter streamWriter = new StreamWriter("../../../Output.txt"))
+            {
+                foreach (KeyValuePair<string, int> word in wordsCount)
+                {
+                    streamWriter.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/07.StreamsFilesAndDirectories/04.MergeFiles/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _04.MergeFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            using (StreamReader reader = new StreamReader("../../../Input1.txt"))
+            {
+                string text = reader.ReadLine();
+                while (text != null)
+                {
+                    numbers.Add(int.Parse(text));
+                    text = reader.ReadLine();
+                }
+                reader.Close();
+            }
+
+            using (StreamReader reader = new StreamReader("../../../Input2.txt"))
+            {
+                string text = reader.ReadLine();
+                while (text != null)
+                {
+                    numbers.Add(int.Parse(text));
+                    text = reader.ReadLine();
+                }
+                reader.Close();
+            }
+
+            numbers.Sort();
+            using (StreamWriter streamWriter = new StreamWriter("../../../Output.txt"))
+            {
+                for (int number = 0; number < numbers.Count; number++)
+                {
+                    streamWriter.WriteLine(numbers[number]);
+                }
+                streamWriter.Close();
+            }
+        }
+    }
+}
