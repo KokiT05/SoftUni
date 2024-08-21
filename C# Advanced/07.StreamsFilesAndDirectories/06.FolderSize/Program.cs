25e542a752f308e763248bea0e9aba57f299ebed
Konstantin Tinev <konstantintinev5@gmail.com>
2024-08-21 22:21:01 +0300

    Add new tasks

---
+++ C# Advanced/07.StreamsFilesAndDirectories/06.FolderSize/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06.FolderSize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = Console.ReadLine();
+
+            string[] files = Directory.GetFiles(folderPath);
+
+            long fileSize = 0;
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                fileSize += fileInfo.Length;
+
+            }
+
+            Console.WriteLine(fileSize / 1024.0 / 1024.0);
+        }
+    }
+}
