73c36aab57a97651102eedf76130d437b9fc03d4
Konstantin Tinev <konstantintinev5@gmail.com>
2024-08-22 19:42:51 +0300

    Add bonus from the topic Streams, Files and Directories

--- C# Advanced/07.StreamsFilesAndDirectories/06.FolderSize/Program.cs
+++ C# Advanced/07.StreamsFilesAndDirectories/06.FolderSize/Program.cs
@@ -7,17 +7,17 @@
             string folderPath = Console.ReadLine();
 
             string[] files = Directory.GetFiles(folderPath);
+            double fileSize = 0;
 
-            long fileSize = 0;
-
+            string[] directories = Directory.GetDirectories(folderPath);
             foreach (string file in files)
             {
                 FileInfo fileInfo = new FileInfo(file);
                 fileSize += fileInfo.Length;
-
+                Console.WriteLine($"Name: {fileInfo.FullName}");
+                Console.WriteLine($"Extension: {fileInfo.Extension}");
+                Console.WriteLine($"Length: {fileInfo.Length}");
             }
-
-            Console.WriteLine(fileSize / 1024.0 / 1024.0);
         }
     }
 }
---
+++ C# Advanced/07.StreamsFilesAndDirectories/07.ScanFolderRecursively/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _07.ScanFolderRecursively
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = Console.ReadLine();
+            ScanFolderRecursively(folderPath, 0);
+        }
+
+        static double ScanFolderRecursively(string folderPath, int indentation)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(folderPath);
+
+                double fileSize = 0;
+
+                string[] directories = Directory.GetDirectories(folderPath);
+
+                foreach (string directory in directories)
+                {
+                    fileSize += ScanFolderRecursively(directory, indentation + 3);
+                }
+
+                foreach (string file in files)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    fileSize += fileInfo.Length;
+                    Console.WriteLine($"{new string(' ', indentation)}{fileInfo.FullName}");
+
+                    //For for a dangerous virus put Fille Delete (file path)
+
+                }
+
+                // Directory Delete (folder path)
+
+                return fileSize / 1024.0 / 1024.0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
