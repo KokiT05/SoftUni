28023f8e97a347b59b9f6fbf587c0fd269f1da26
Konstantin Tinev <konstantintinev5@gmail.com>
2024-08-23 18:31:59 +0300

    Add new topic Exercise Stream, Files and Directoties

---
+++ C# Advanced/08.ExerciseStreamsFilesAndDirectories/01.EvenLines/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _01.EvenLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] sumbols = { '-', ',', '.', '!', '?' };
+
+            using (StreamReader streamReader = new StreamReader("../../../text.txt"))
+            {
+                int count = 0;
+                string textFromFile = streamReader.ReadLine();
+                while (textFromFile != null)
+                {
+                    if (count % 2 == 0)
+                    {
+                        textFromFile = ReplaceSumbols(sumbols, textFromFile);
+                        textFromFile = ReverseString(textFromFile);
+                        Console.WriteLine(textFromFile);
+                    }
+                    count++;
+                    textFromFile = streamReader.ReadLine();
+                }
+            }
+        }
+
+        static string ReplaceSumbols(char[] chars, string text)
+        {
+            for (int i = 0; i < chars.Length; i++)
+            {
+                text = text.Replace(chars[i], '@');
+            }
+
+            return text;
+        }
+
+        static string ReverseString(string text)
+        {
+            string[] stringArray = text.Split();
+            string reverseString = string.Empty;
+
+            for (int i = stringArray.Length - 1; i >= 0; i--)
+            {
+                reverseString += stringArray[i] + " ";
+            }
+            reverseString = reverseString.TrimEnd();
+            return reverseString;
+        }
+    }
+}
---
+++ C# Advanced/08.ExerciseStreamsFilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace _02.LineNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] punctuationMarks = new char[] { '.', ',', '!', '?', '\'', '-' };
+            int countPunctuationMarks = 0;
+            int countLetters = 0;
+
+            string[] text = File.ReadLines("../../../text.txt").ToArray();
+            string[] result = new string[text.Length];
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < text[i].Length; j++)
+                {
+                    countPunctuationMarks = CountPunctuationMarks(punctuationMarks, text[i]);
+                    countLetters = CountLetters(text[i]);
+                }
+
+                text[i] = $"Line {i + 1}: " + text[i] + $" ({countLetters})({countPunctuationMarks})";
+                result[i] = text[i];
+            }
+
+            File.WriteAllLines("../../../output.txt", result);
+        }
+
+        static int CountPunctuationMarks(char[] punctuationMarks, string text)
+        {
+            int countOfPunctuationMarks = 0;
+            for (int i = 0; i < punctuationMarks.Length; i++)
+            {
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (punctuationMarks[i] == text[j])
+                    {
+                        countOfPunctuationMarks++;
+                    }
+                }
+            }
+
+            return countOfPunctuationMarks;
+        }
+
+        static int CountLetters(string text)
+        {
+            int countOfLetters = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    countOfLetters++;
+                }
+            }
+
+            return countOfLetters;
+        }
+    }
+}
