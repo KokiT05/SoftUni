44162ebd9355ab7a96fbf9a19a0afedeba1bf0b6
Konstantin Tinev <konstantintinev5@gmail.com>
2024-09-18 03:40:00 +0300

    Add new topic

---
+++ C# Advanced/12.ExerciseDefiningclasses/01.DefineAClassPerson/Family.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            this.People = new List<Person>();
+        }
+
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person person)
+        {
+            People.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            People = People.OrderByDescending(p => p.Age).ToList();
+            return People.First();
+        }
+    }
+}
---
+++ C# Advanced/12.ExerciseDefiningclasses/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace DefiningClasses
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+            Person person = new Person();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputData = Console.ReadLine().Split(' ');
+                string name = inputData[0];
+                int age = int.Parse(inputData[1]);
+
+                person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            person = family.GetOldestMember();
+            Console.WriteLine($"{person.Name} {person.Age}");
+        }
+    }
+}
