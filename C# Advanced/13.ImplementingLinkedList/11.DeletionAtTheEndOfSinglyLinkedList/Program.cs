3d6486802132bd6a6eda253ebb05bb8551981bcb
Konstantin Tinev <konstantintinev5@gmail.com>
2024-10-01 18:15:13 +0300

    Add new tasks

---
+++ C# Advanced/13.ImplementingLinkedList/11.DeletionAtTheEndOfSinglyLinkedList/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _11.DeletionAtTheEndOfSinglyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<Node> linkedList = new List<Node>();
+            Node head = new Node(inputNumbers[0]);
+            Node currentNode = head;
+            linkedList.Add(head);
+
+            for (int i = 0; i < inputNumbers.Length - 1; i++)
+            {
+                Node node = linkedList[i];
+                node.Next = new Node(inputNumbers[i + 1]);
+                linkedList.Add(node.Next);
+            }
+
+            string command = Console.ReadLine();
+            while (command != "end" && head != null)
+            {
+                if (command == "delete last")
+                {
+                    if (head.Next == null)
+                    {
+                        head = null;
+                        break;
+                    }
+
+                    currentNode = head;
+                    while (currentNode.Next.Next != null)
+                    {
+                        currentNode = currentNode.Next;
+                    }
+
+                    currentNode.Next = null;
+                }
+                else if (command == "print list")
+                {
+                    currentNode = head;
+                    while (currentNode != null)
+                    {
+                        Console.WriteLine(currentNode.Value);
+                        currentNode = currentNode.Next;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            while (head.Next != null)
+            {
+                head = head.Next;
+            }
+
+            Console.WriteLine($"Last element value: {head.Value}");
+        }
+    }
+}
---
+++ C# Advanced/13.ImplementingLinkedList/12.DeletionAtASpecificPositionOfSinglyLinkedList/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Xml;
+
+namespace _12.DeletionAtASpecificPositionOfSinglyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<Node> linkedList = new List<Node>();
+            Node head = new Node(inputNumbers[0]);
+            linkedList.Add(head);
+
+            int length = 0;
+            for (int i = 0; i < inputNumbers.Length - 1; i++)
+            {
+                length++;
+                Node node = linkedList[i];
+                node.Next = new Node(inputNumbers[i + 1]);
+                linkedList.Add(node.Next);
+            }
+
+            int position = int.Parse(Console.ReadLine());
+
+            if (position > length)
+            {
+                Console.WriteLine("Invalid position!");
+                return;
+            }
+            length = 1;
+
+            Node currentNode = head;
+            Node previous = null;
+            Node current = null;
+            while (currentNode != null)
+            {
+                if (length == position)
+                {
+                    current = currentNode.Next;
+                    currentNode = null;
+                    previous.Next = current;
+                    break;
+                }
+                else
+                {
+                    previous = currentNode;
+                    currentNode = currentNode.Next;
+                }
+                length++;
+            }
+
+            while (head != null)
+            {
+                Console.Write(head.Value + "->");
+                head = head.Next;
+            }
+        }
+    }
+}
