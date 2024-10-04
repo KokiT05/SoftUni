ecad79193bb23046255f59294c731b125bb7100d
Konstantin Tinev <konstantintinev5@gmail.com>
2024-10-04 21:34:28 +0300

    last task solved

---
+++ C# Advanced/14.ImplementingStackAndQueue/14.ImplementingStackAndQueue/CustomStack.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.ImplementingStackAndQueue
+{
+    public class CustomStack
+    {
+        private const int initialCapacity = 4;
+        private int[] items;
+        private int count;
+
+        public CustomStack()
+        {
+            this.count = 0;
+            this.items = new int[initialCapacity];
+        }
+
+        public int Count { get { return this.count; } }
+
+        public void Push(int element)
+        {
+            if (this.items.Length == this.count)
+            {
+                Resize();
+            }
+
+            this.items[this.count] = element;
+            this.count++;
+        }
+
+        public int Pop()
+        {
+            Validate();
+
+            int lastIndex = this.count - 1;
+            int lastValue = this.items[lastIndex];
+            this.items[lastIndex] = default(int);
+            this.count--;
+
+            if (this.items.Length / 4 == this.count)
+            {
+                Shrink();
+            }
+
+            return lastValue;
+        }
+
+        public int Peek()
+        {
+            Validate();
+
+            int lastValue = this.items[this.count - 1];
+
+            return lastValue;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            for (int i = this.count - 1; i >= 0; i--)
+            {
+                action(this.items[i]);
+            }
+        }
+
+        public void MySelect(Func<int, int> func)
+        {
+            for (int i = this.count - 1; i >= 0; i--)
+            {
+                this.items[i] = func(this.items[i]);
+                //int element = this.items[i];
+                //int result = func(element);
+                //this.items[i] = result;
+            }
+        }
+
+        private void Validate()
+        {
+            if (this.count == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty!");
+            }
+        }
+
+        private void Shrink()
+        {
+            int[] copy = new int[this.items.Length / 2];
+            Array.Copy(this.items, copy, this.count);
+            this.items = copy;
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[this.items.Length * 2];
+            Array.Copy(this.items, copy, this.count);
+            this.items = copy;
+        }
+    }
+}
--- C# Advanced/14.ImplementingStackAndQueue/14.ImplementingStackAndQueue/Program.cs
+++ C# Advanced/14.ImplementingStackAndQueue/14.ImplementingStackAndQueue/Program.cs
@@ -1,38 +1,65 @@
-﻿namespace _14.ImplementingStackAndQueue
+﻿using System.Threading.Channels;
+
+namespace _14.ImplementingStackAndQueue
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            CustomList customList = new CustomList();
+            //CustomList customList = new CustomList();
 
-            customList.Add(1);
-            customList.Add(2);
-            customList.Add(3);
-            customList.Add(4);
+            //customList.Add(1);
+            //customList.Add(2);
+            //customList.Add(3);
+            //customList.Add(4);
 
-            Console.WriteLine(customList.Count);
-            for (int i = 0; i < customList.Count; i++)
-            {
-                Console.Write(customList[i] + " ");
-            }
-            Console.WriteLine();
-            customList.Insert(1, 2222);
+            //Console.WriteLine(customList.Count);
+            //for (int i = 0; i < customList.Count; i++)
+            //{
+            //    Console.Write(customList[i] + " ");
+            //}
+            //Console.WriteLine();
+            //customList.Insert(1, 2222);
 
-            Console.WriteLine(customList.Count);
-            for (int i = 0; i < customList.Count; i++)
-            {
-                Console.Write(customList[i] + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine(customList.Contains(1));
-            Console.WriteLine(customList.Contains(1123));
+            //Console.WriteLine(customList.Count);
+            //for (int i = 0; i < customList.Count; i++)
+            //{
+            //    Console.Write(customList[i] + " ");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine(customList.Contains(1));
+            //Console.WriteLine(customList.Contains(1123));
 
-            customList.Swap(0, 4);
-            for (int i = 0; i < customList.Count; i++)
-            {
-                Console.Write(customList[i] + " ");
-            }
+            //customList.Swap(0, 4);
+            //for (int i = 0; i < customList.Count; i++)
+            //{
+            //    Console.Write(customList[i] + " ");
+            //}
+
+            CustomStack customStack = new CustomStack();
+            customStack.Push(6);
+            customStack.Push(7);
+            customStack.Push(8);
+            customStack.Push(9);
+
+            customStack.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("Length: " + customStack.Count);
+
+            Console.WriteLine($"Last element {customStack.Peek()}");
+            Console.WriteLine($"Pop element {customStack.Pop()}");
+            Console.WriteLine($"Last element {customStack.Peek()}");
+            customStack.Push(32);
+            customStack.Push(3122);
+
+            customStack.ForEach(e => Console.WriteLine(e));
+
+            Console.WriteLine($"Last element {customStack.Peek()}");
+            customStack.Pop();
+            customStack.Pop();
+            customStack.Pop();
+
+            customStack.MySelect(n => n * 2);
+            customStack.ForEach(x => Console.WriteLine(x));
         }
     }
 }
