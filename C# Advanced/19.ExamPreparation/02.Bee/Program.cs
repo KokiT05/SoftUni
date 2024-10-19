2fe1f46d007d807e4d8c418eedfdb0d0f6c9c980
Konstantin Tinev <konstantintinev5@gmail.com>
2024-10-19 20:56:57 +0300

    Add last exam tasks

---
+++ C# Advanced/19.ExamPreparation/02.Bee/Program.cs
@@ -0,0 +1,113 @@
+﻿namespace _02.Bee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfTerritory = int.Parse(Console.ReadLine());
+            char[,] territory = new char[sizeOfTerritory, sizeOfTerritory];
+
+            int beeRow = 0;
+            int beeCol = 0;
+            int polinatedFlower = 0;
+
+            for (int i = 0; i < sizeOfTerritory; i++)
+            {
+                string inputRow = Console.ReadLine();
+                for (int j = 0; j < inputRow.Length; j++)
+                {
+                    if (inputRow[j] == 'B')
+                    {
+                        beeRow = i;
+                        beeCol = j;
+                    }
+                    territory[i,j] = inputRow[j];
+                }
+            }
+            bool isLost = false;
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                territory[beeRow, beeCol] = '.';
+                beeRow = MoveBeeRow(beeRow, command);
+                beeCol = MoveBeeCol(beeCol, command);
+
+                isLost = (beeRow == sizeOfTerritory || beeRow < 0) ||
+                         (beeCol == sizeOfTerritory || beeCol < 0);
+                if (isLost)
+                {
+                    Console.WriteLine("The bee got lost!");
+                    break;
+                }
+
+                if (territory[beeRow, beeCol] == 'f')
+                {
+                    territory[beeRow, beeCol] = 'B';
+                    polinatedFlower++;
+                }
+                else if (territory[beeRow, beeCol] == 'O')
+                {
+                    continue;
+                }
+
+                territory[beeRow, beeCol] = 'B';
+
+                command = Console.ReadLine();
+            }
+
+            int needPolinatedFlower = 5;
+            if (polinatedFlower >= needPolinatedFlower)
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {polinatedFlower} flowers!");
+            }
+            else
+            {
+                Console
+                .WriteLine
+                ($"The bee couldn't pollinate the flowers, she needed {needPolinatedFlower - polinatedFlower} flowers more");
+            }
+
+            PrintTerritory(sizeOfTerritory, territory);
+        }
+
+        static void PrintTerritory(int sizeOfTerritory, char[,] territory)
+        {
+            for (int i = 0; i < sizeOfTerritory; i++)
+            {
+                for (int j = 0; j < sizeOfTerritory; j++)
+                {
+                    Console.Write($"{territory[i, j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int MoveBeeRow(int beeRow, string command)
+        {
+            if (command == "up")
+            {
+                beeRow--;
+            }
+            else if (command == "down")
+            {
+                beeRow++;
+            }
+
+            return beeRow;
+        }
+
+        static int MoveBeeCol(int beeCol, string command)
+        {
+            if (command == "right")
+            {
+                beeCol++;
+            }
+            else if (command == "left")
+            {
+                beeCol--;
+            }
+
+            return beeCol;
+        }
+    }
+}
--- C# Advanced/19.ExamPreparation/19.ExamPreparation/Program.cs
+++ C# Advanced/19.ExamPreparation/19.ExamPreparation/Program.cs
@@ -4,17 +4,8 @@
     {
         static void Main(string[] args)
         {
-            int[] liliesInput = Console.ReadLine()
-                                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
-                                    .Select(int.Parse)
-                                    .ToArray();
-            Stack<int> lilies = new Stack<int>(liliesInput);
-
-            int[] rosesInput = Console.ReadLine()
-                                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
-                                .Select(int.Parse)
-                                .ToArray();
-            Queue<int> roses = new Queue<int>(rosesInput);
+            Stack<int> lilies = new Stack<int>(LiliesRead());
+            Queue<int> roses = new Queue<int>(RosesInput());
 
             int storedFlowers = 0;
             int countOfWreaths = 0;
@@ -46,18 +37,43 @@
             }
 
             int needWreath = 5;
-            if (countOfWreaths >= 5)
+            int leftWreath = storedFlowers / 15;
+            countOfWreaths += leftWreath;
+            PrintWreathMessage(countOfWreaths, needWreath);
+        }
+
+        static void PrintWreathMessage(int countOfWreaths, int needWreath)
+        {
+            if (countOfWreaths >= needWreath)
             {
                 Console
-                .WriteLine($"You made it, you are going to the competition with {countOfWreaths} wreaths!" );
+                .WriteLine($"You made it, you are going to the competition with {countOfWreaths} wreaths!");
             }
             else
             {
-                int leftWreath = storedFlowers / 15;
-                countOfWreaths += leftWreath;
                 Console
-                .WriteLine($"You didn't make it, you need {needWreath - countOfWreaths} wreaths more");
+                .WriteLine($"You didn't make it, you need {needWreath - countOfWreaths} wreaths more!");
             }
+        }
+
+        static int[] LiliesRead()
+        {
+             int[] liliesInput = Console.ReadLine()
+                        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+            return liliesInput;
+        }
+
+        static int[] RosesInput()
+        {
+            int[] rosesInput = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+            return rosesInput;
         }
     }
 }
