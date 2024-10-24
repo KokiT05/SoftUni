d0a1fe715eb8fd5bd170c775d9018fb37a9d64c5
Konstantin Tinev <konstantintinev5@gmail.com>
2024-10-24 23:01:02 +0300

    Add new task: Bubble Sort Algorithms

---
+++ C# Advanced/20.AlgorithmsIntroduction/08.SortBubbleSort/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _08.SortBubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 9, 3, 14, 41, 123, 2, 8, 5 };
+            bool swapped = false;
+
+            Console.WriteLine($"Before Bubble srot");
+            Console.WriteLine(string.Join(", ", array));
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"After Bubble sort");
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
