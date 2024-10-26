38a551c780587447f4f12e63d0b05cdd73a6ba69
Konstantin Tinev <konstantintinev5@gmail.com>
2024-10-26 03:47:15 +0300

    Add new sort algorithm: SelectionSort

---
+++ C# Advanced/20.AlgorithmsIntroduction/011.SortSelectionSort/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _011.SortSelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 65, 25, 12, 22, 11 };
+            Console.WriteLine($"Before SelectionSort: ");
+            Console.WriteLine(string.Join(", ", array));
+
+            Sort(array);
+            Console.WriteLine($"After SelectionSort: ");
+            Console.WriteLine(string.Join(", ", array));
+        }
+
+        public static void Sort(int[] array)
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                //if (array[minIndex] != array[i])
+                //{
+                //    array[minIndex] = array[minIndex] ^ array[i];
+                //    array[i] = array[minIndex] ^ array[i];
+                //    array[minIndex] = array[minIndex] ^ array[i];
+                //}
+
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+}
--- C# Advanced/20.AlgorithmsIntroduction/09.SortMergeSort/Program.cs
+++ C# Advanced/20.AlgorithmsIntroduction/09.SortMergeSort/Program.cs
@@ -19,28 +19,22 @@
 
             int mid = array.Length / 2;
 
-            // Създаваме леви и десни подмасиви
             int[] left = new int[mid];
             int[] right = new int[array.Length - mid];
 
             Array.Copy(array, 0, left, 0, mid);
             Array.Copy(array, mid, right, 0, array.Length - mid);
 
-            // Рекурсивно сортиране на подмасивите
             Sort(left);
             Sort(right);
 
-            // Сливане на сортираните подмасиви
             Merge(array, left, right);
         }
 
         private static void Merge(int[] array, int[] left, int[] right)
         {
             int i = 0, j = 0, k = 0;
-            // 1 23 L - 2
-            // 4 15 L - 2
-            // 1 23 5 4
-            // Сливане на подмасивите в сортиран ред
+
             while (i < left.Length && j < right.Length)
             {
                 if (left[i] <= right[j])
@@ -56,7 +50,6 @@
                 k++;
             }
 
-            // Копиране на оставащите елементи, ако има такива
             while (i < left.Length)
             {
                 array[k] = left[i];
