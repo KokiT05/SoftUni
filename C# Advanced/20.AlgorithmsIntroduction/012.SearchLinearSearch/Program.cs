6f46b0d990ef25acacdd79de64b325e50a75d6ee
Konstantin Tinev <konstantintinev5@gmail.com>
2024-10-26 18:21:47 +0300

    Add new two algorithms: LinearSearch and BinarySearch

---
+++ C# Advanced/20.AlgorithmsIntroduction/012.SearchLinearSearch/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _012.SearchLinearSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 94, 41, 51, 3, 8, 12, 17, 25, 33, };
+            int target = 7;
+            int result = LinearSearch(array, target);
+
+            if (result > 0)
+            {
+                Console.WriteLine($"Елементът {target} е намерен на индекс {result}");
+            }
+            else
+            {
+                Console.WriteLine($"Елементътъ {target} не е намерен");
+            }
+
+        }
+
+        public static int LinearSearch(int[] array, int target)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == target)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ C# Advanced/20.AlgorithmsIntroduction/013.SearchBinarySearch/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _013.SearchBinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 5, 7, 9, 11, 13 };
+            int target = 41;
+            int result = BinarySearch(arr, target);
+
+            if (result > 0)
+            {
+                Console.WriteLine($"Елементът: {target} е на индекс: {result}");
+            }
+            else
+            {
+                Console.WriteLine($"Елементът: {target} не е намерен");
+            }
+
+
+            //Console.WriteLine("Hello, World!");
+        }
+
+        public static int BinarySearch(int[] array, int target)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (array[mid] == target)
+                {
+                    return mid;
+                }
+                else if (array[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
