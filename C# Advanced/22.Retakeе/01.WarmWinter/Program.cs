0a9675d26b588afb9e2e596e3f6ede4dd54c26e6
Konstantin Tinev <konstantintinev5@gmail.com>
2024-10-31 23:24:06 +0200

    Add first task from the topic: 22.Ratake

--- C# Advanced/22.Retakeе/01.WarmWinter/Program.cs
+++ C# Advanced/22.Retakeе/01.WarmWinter/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
+            Stack<int> hats = new Stack<int>(Console.ReadLine()
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToArray());
 
+            Queue<int> scarfs = new Queue<int>(Console.ReadLine()
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToArray());
+
+            List<int> sets = new List<int>();
+
+            while (hats.Count > 0 && scarfs.Count > 0)
+            {
+                int hat = hats.Peek();
+                int scarf = scarfs.Peek();
+
+                if (hat > scarf)
+                {
+                    sets.Add(hat+scarf);
+                    hats.Pop();
+                    scarfs.Dequeue();
+                }
+
+                if (scarf > hat)
+                {
+                    hats.Pop();
+                    continue;
+                }
+
+                if (scarf == hat)
+                {
+                    scarfs.Dequeue();
+                    hat = hats.Pop();
+                    hats.Push(hat+1);
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.Max()}");
+            Console.WriteLine(string.Join(' ', sets));
         }
     }
 }
