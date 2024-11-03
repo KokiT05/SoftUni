0329d8f171c43b63c63ff89663bd6d32735a185d
Konstantin Tinev <konstantintinev5@gmail.com>
2024-11-03 23:33:08 +0200

    Add new topic: 04.ExerciseEncapsulation

---
+++ C# OOP/04.ExerciseEncapsulation/03.ShoppingSpree/Person.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private readonly List<Product> bagProducts;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            bagProducts = new List<Product>();
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public IReadOnlyList<Product> BagProducts
+        {
+            get { return this.bagProducts.AsReadOnly(); }
+        }
+
+
+        public void Bought(Product product)
+        {
+            this.bagProducts.Add(product);
+            this.Money -= product.Cost;
+        }
+
+        public string PrintBag()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < bagProducts.Count; i++)
+            {
+                if (i == bagProducts.Count - 1)
+                {
+                    stringBuilder.Append($"{bagProducts[i].Name}");
+                }
+                else
+                {
+                    stringBuilder.Append($"{bagProducts[i].Name}, ");
+                }
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
---
+++ C# OOP/04.ExerciseEncapsulation/03.ShoppingSpree/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Net.Http.Headers;
+
+namespace _03.ShoppingSpree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] personInput = Console.ReadLine().Split(new char[] { ';', '=' });
+            string[] productsInput = Console.ReadLine().Split(new char[] {';', '='});
+
+            Dictionary<string, Person> personBags = new Dictionary<string, Person>();
+            Dictionary<string, Product> productCost = new Dictionary<string, Product>();
+
+            try
+            {
+                Person person;
+                for (int i = 0; i < personInput.Length - 1; i = i + 2)
+                {
+                    string personName = personInput[i];
+                    decimal personMoney = decimal.Parse(personInput[i + 1]);
+                    person = new Person(personName, personMoney);
+                    personBags.Add(personName, person);
+                }
+
+                Product product;
+                for (int i = 0; i < productsInput.Length - 1; i = i + 2)
+                {
+                    string productName = productsInput[i];
+                    decimal cost = decimal.Parse(productsInput[i + 1]);
+                    product = new Product(productName, cost);
+                    productCost.Add(productName, product);
+                }
+            }
+            catch (ArgumentException messageException)
+            {
+                throw new ArgumentException(messageException.Message);
+            }
+
+            Person currentPerson;
+            Product currentProduct;
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] information = input.Split();
+                string personName = information[0];
+                string productName = information[1];
+
+                if (!personBags.ContainsKey(personName) || 
+                    !productCost.ContainsKey(productName))
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                currentPerson = personBags[personName];
+                currentProduct = productCost[productName];
+
+                if (currentPerson.Money >= currentProduct.Cost)
+                {
+                    Console.WriteLine($"{currentPerson.Name} bought {currentProduct.Name}");
+                    currentPerson.Bought(currentProduct);
+                }
+                else
+                {
+                    Console.WriteLine
+                        ($"{currentPerson.Name} can't afford {currentProduct.Name}");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (Person personBag in personBags.Values)
+            {
+                if (personBag.BagProducts.Count == 0)
+                {
+                    Console.WriteLine($"{personBag.Name} - Nothing bought");
+                    continue;
+                }
+                Console.WriteLine($"{personBag.Name} - {personBag.PrintBag()}");
+            }
+        }
+    }
+}
