de0d9edef9c27de951b065c08eed05183e1fd636
Konstantin Tinev <konstantintinev5@gmail.com>
2024-11-06 09:07:32 +0200

    Add last task from the topic: 05.Encapsulation

---
+++ C# OOP/04.ExerciseEncapsulation/05.FootballTeamGenerator/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace _05.FootballTeamGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Team> teams = new Dictionary<string, Team>();
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] inputInformation = input.Split(';');
+                string command = inputInformation[0];
+                string teamName = inputInformation[1];
+
+                try
+                {
+                    if (command == "Team")
+                    {
+                        AddTeam(teamName, teams);
+                    }
+                    else if (command == "Add")
+                    {
+                        if (!teams.ContainsKey(teamName))
+                        {
+                            throw new ArgumentException($"Team {teamName} does not exist.");
+                        }
+
+                        string playerName = inputInformation[2];
+                        int endurance = int.Parse(inputInformation[3]);
+                        int sprint = int.Parse(inputInformation[4]);
+                        int dribble = int.Parse(inputInformation[5]);
+                        int passing = int.Parse(inputInformation[6]);
+                        int shooting = int.Parse(inputInformation[7]);
+
+                        AddPlayerInTeam(teams[teamName], playerName,
+                                                        endurance,
+                                                        sprint,
+                                                        dribble,
+                                                        passing,
+                                                        shooting);
+                    }
+                    else if (command == "Remove")
+                    {
+                        string playerName = inputInformation[2];
+                        RemovePlayerFromTeam(playerName, teams[teamName]);
+                    }
+                    else if (command == "Rating")
+                    {
+                        if (!teams.ContainsKey(teamName))
+                        {
+                            throw new ArgumentException($"Team {teamName} does not exist.");
+                        }
+
+                        Console.WriteLine
+                            ($"{teams[teamName].Name} - {teams[teamName].Rating}");
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+
+                input = Console.ReadLine();
+            }
+
+
+        }
+
+        public static void AddTeam(string teamName, Dictionary<string, Team> teams)
+        {
+            Team team = new Team(teamName);
+            teams.Add(teamName, team);
+        }
+
+        public static void AddPlayerInTeam(Team team, 
+                                            string playerName,
+                                            params int[] playerStats)
+        {
+
+            Player player = new Player
+                                (playerName, 
+                                playerStats[0], 
+                                playerStats[1], 
+                                playerStats[2],
+                                playerStats[3],
+                                playerStats[4]);
+
+            team.AddPlayer(player);
+        }
+
+        public static void RemovePlayerFromTeam(string playerName, Team team)
+        {
+            team.RemovePlayer(playerName);
+        }
+    }
+}
---
+++ C# OOP/04.ExerciseEncapsulation/05.FootballTeamGenerator/Team.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.FootballTeamGenerator
+{
+    public class Team
+    {
+        private string name;
+        private readonly List<Player> players;
+        public Team(string name)
+        {
+            this.Name = name;
+            this.players = new List<Player>();
+        }
+        public int NumberOfPlayers => this.players.Count;
+
+        public IReadOnlyList<Player> Players
+        {
+            get { return this.players.AsReadOnly(); }
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+
+                this.name = value;
+            }
+        }
+        public int Rating => this.TotalRating();
+
+        public void AddPlayer(Player player)
+        {
+            this.players.Add(player);
+        }
+
+        public void RemovePlayer(string playerName)
+        {
+            if (!this.players.Any(n => n.Name == playerName))
+            {
+                throw new ArgumentException
+                    ($"Player {playerName} is not in {this.Name} team.");
+            }
+
+            Player player = this.players.FirstOrDefault(n => n.Name == playerName);
+            this.players.Remove(player);
+        }
+
+        private int TotalRating()
+        {
+            int totalRating = 0;
+            foreach (Player player in this.Players)
+            {
+                totalRating += player.SkillLevel;
+            }
+
+            return totalRating;
+        }
+    }
+}
