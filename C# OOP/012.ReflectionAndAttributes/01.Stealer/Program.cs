1383544547bd4fb7b6c36f304061c1c01ce92160
Konstantin Tinev <konstantintinev5@gmail.com>
2024-12-22 22:55:29 +0200

    Add new topic: 12. Reflection and Attributes

---
+++ C# OOP/012.ReflectionAndAttributes/01.Stealer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01.Stealer // Stealer
+{
+    internal class Program // StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+
+            string result = spy.StealFieldInfo("_01.Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C# OOP/012.ReflectionAndAttributes/01.Stealer/Spy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fields)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] fieldsInfo = classType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic
+                | BindingFlags.Static | BindingFlags.Public);
+
+            StringBuilder result = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] {});
+
+            result.AppendLine($"Class under investigation: {className}");
+
+            foreach (FieldInfo field in fieldsInfo.Where(f => fields.Contains(f.Name)))
+            {
+                result.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
