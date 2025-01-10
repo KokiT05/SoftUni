8cfae283c1ce0b18c72fc3d38a868bb8cd45b2d7
Konstantin Tinev <konstantintinev5@gmail.com>
2025-01-10 19:18:19 +0200

    Add task: 5 Mocking from the topic: Unit Testing

--- C# OOP/014.UnitTesting/Skeleton.Tests/HeroTests.cs
+++ C# OOP/014.UnitTesting/Skeleton.Tests/HeroTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using Skeleton.Contracts;
 using Skeleton.Tests.Fakes;
 
 [TestFixture]
@@ -7,13 +9,26 @@ public class HeroTests
     [Test]
     public void WhenTargetDies_ShouldReceiveExperience()
     {
-        FakeTarget target = new FakeTarget();
-        FakeWeapon fakeWeapon = new FakeWeapon();
+        //FakeTarget target = new FakeTarget();
+        //FakeWeapon fakeWeapon = new FakeWeapon();
 
-        Hero hero = new Hero("HeroTest", fakeWeapon);
+        //Hero hero = new Hero("HeroTest", fakeWeapon);
 
-        hero.Attack(target);
+        //hero.Attack(target);
 
-        Assert.That(hero.Experience, Is.EqualTo(target.GiveExperience()));
+        //Assert.That(hero.Experience, Is.EqualTo(target.GiveExperience()));
+
+        Mock<ITarget> target = new Mock<ITarget>();
+        Mock<IWeapon> weapon = new Mock<IWeapon>();
+
+        target.Setup(t => t.IsDead()).Returns(true);
+        target.Setup(t => t.GiveExperience())
+                .Returns(20);
+
+        Hero hero = new Hero("Ivan", weapon.Object);
+
+        hero.Attack(target.Object);
+
+        Assert.That(hero.Experience, Is.EqualTo(target.Object.GiveExperience()));
     }
 }
