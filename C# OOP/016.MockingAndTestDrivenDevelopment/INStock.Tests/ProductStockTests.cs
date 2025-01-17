6929dbeef1dea8ac7f0a3560bcef99a0ef993a11
Konstantin Tinev <konstantintinev5@gmail.com>
2025-01-17 11:53:40 +0200

    Add new tests in the topic: 016.Mocking and test driven development

--- C# OOP/016.MockingAndTestDrivenDevelopment/INStock.Tests/ProductStockTests.cs
+++ C# OOP/016.MockingAndTestDrivenDevelopment/INStock.Tests/ProductStockTests.cs
@@ -6,6 +6,8 @@
     using INStock.Fake;
     using Moq;
     using NUnit.Framework;
+    using NUnit.Framework.Constraints;
+
     [TestFixture]
     public class ProductStockTests
     {
@@ -20,6 +22,9 @@
         Mock<IProduct> thirdProduct;
         Mock<IProduct> mockProduct;
         Mock<IProductStock> mockProductStock;
+
+        Market market;
+
         [SetUp]
         public void Setup()
         {
@@ -38,7 +43,10 @@
             this.thirdProduct.Setup(l => l.Label).Returns("ThirdProduct");
             this.thirdProduct.Setup(p => p.Price).Returns(333M);
             this.thirdProduct.Setup(q => q.Quantity).Returns(33);
+
             this.mockProductStock = new Mock<IProductStock>();
+
+            this.market = new Market(this.mockProductStock.Object);
 
             // Fake product and fake productStock
             this.fakeProductStock = new FakeProductStockSystem();
@@ -48,15 +56,15 @@
             this.fakeProductStock.Add(this.fakeProduct);
 
             // Mock SetUp
-            this.mockProductStock.Setup(ps => ps.Add(It.IsAny<IProduct>()))
-                    .Callback(() =>
-                    {
-                        this.mockProductStock.Object.Add(this.mockProduct.Object);
-                    });
+            //this.mockProductStock.Setup(ps => ps.Add(It.IsAny<IProduct>()))
+            //        .Callback(() =>
+            //        {
+            //            this.mockProductStock.Object.Add(this.mockProduct.Object);
+            //        });
 
-            this.mockProductStock.Setup(ps => ps.Contains(this.mockProduct.Object)).Returns(true);
+            //this.mockProductStock.Setup(ps => ps.Contains(this.mockProduct.Object)).Returns(true);
 
-            this.mockProductStock.Setup(ps => ps.Count).Returns(1);
+            //this.mockProductStock.Setup(ps => ps.Count).Returns(1);
 
             //this.mockProductStock.Setup(ps => ps[It.IsAny<int>()]).Returns(this.mockProduct.Object);
 
@@ -183,6 +191,173 @@
         public void FakeIndexShouldReturnFakeProduct()
         {
             Assert.That(this.fakeProductStock[0], Is.EqualTo(this.fakeProduct));
+        }
+
+        //Mock tests
+
+        [Test]
+        public void MockCountPropertyShouldReturnCountOfProduct()
+        {
+            this.mockProductStock.Setup(ps => ps.Count).Returns(10);
+
+            Assert.That(this.market.CurrentProductCount(), Is.EqualTo(10));
+        }
+
+        [Test]
+        public void MockGetProductByIndex()
+        {
+            int index = 0;
+            this.mockProductStock.Setup(ps => ps[index]).Throws(new ArgumentOutOfRangeException());
+
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                IProduct product = this.market[index];
+            });
+
+            index = 1;
+            this.mockProductStock.Setup(ps => ps[index]).Returns(this.mockProduct.Object);
+
+            Assert.That(this.mockProductStock.Object[index], Is.EqualTo(this.mockProduct.Object));
+        }
+
+        [Test]
+        public void MockContainsMethodShouldReturnTrue()
+        {
+            this.mockProductStock.Setup(ps => ps.Contains(this.mockProduct.Object)).Returns(true);
+
+            Assert.That(this.mockProductStock.Object.Contains(this.mockProduct.Object), Is.EqualTo(true));
+
+            Assert.That(this.mockProductStock.Object.Contains(this.secondProduct.Object), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void MockAddMethodShouldAddProduct()
+        {
+            this.mockProductStock.Object.Add(this.mockProduct.Object);
+
+            this.mockProductStock.Verify(ps => ps.Add(this.mockProduct.Object), Times.Once);
+
+            this.mockProductStock.Setup(ps => ps.Add(It.IsAny<IProduct>()))
+                                    .Callback(() =>
+                                    {
+                                        this.mockProductStock.Setup(ps => ps.Count).Returns(10);
+
+                                    });
+
+            this.mockProductStock.Object.Add(this.mockProduct.Object);
+
+            Assert.That(this.mockProductStock.Object.Count, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void MockRemoveMethodShouldRemoveProduct()
+        {
+            this.mockProductStock.Object.Remove(this.mockProduct.Object);
+
+            this.mockProductStock.Verify(ps => ps.Remove(this.mockProduct.Object), Times.Once);
+
+            this.mockProductStock.Setup(ps => ps.Remove(It.IsAny<IProduct>()))
+                        .Callback(() =>
+                        {
+                            this.mockProductStock.Setup(ps => ps.Count).Returns(9);
+
+                        });
+
+            this.mockProductStock.Object.Remove(this.thirdProduct.Object);
+
+            Assert.That(this.mockProductStock.Object.Count, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void MockFindMethodShouldFindProduct()
+        {
+            int index = 0;
+            this.mockProductStock.Setup(ps => ps.Find(index)).Returns(this.mockProduct.Object);
+
+            Assert.That(this.mockProductStock.Object.Find(index), Is.EqualTo(this.mockProduct.Object));
+
+            index = 100;
+            Assert.That(this.mockProductStock.Object.Find(index), Is.EqualTo(null));
+        }
+
+        [Test]
+        public void MockFindByLabelShouldFindProductByLabel()
+        {
+            this.mockProductStock.Setup(ps => ps.FindByLabel(this.productLabel)).Returns(this.mockProduct.Object);
+
+            Assert.That(this.mockProductStock.Object.FindByLabel(this.productLabel), 
+                Is.EqualTo(this.mockProduct.Object));
+
+            string testLabel = "NoLabel";
+            Assert.That(this.mockProductStock.Object.FindByLabel(testLabel), Is.EqualTo(null));
+        }
+
+        [Test]
+        public void MockFindMostExpensiveProductShouldReturnProduct()
+        {
+            this.mockProductStock.Setup(ps => ps.FindMostExpensiveProduct()).Returns(this.mockProduct.Object);
+
+            Assert.That(this.mockProductStock.Object.FindMostExpensiveProduct(),
+                Is.EqualTo(this.mockProduct.Object));
+        }
+
+        [Test]
+        public void MockFindAllInRangeShouldReturnAllProductInRange()
+        {
+            this.mockProductStock.Setup(ps => ps.FindAllInRange(100, 400))
+                .Returns(new List<IProduct>() { this.secondProduct.Object, this.thirdProduct.Object });
+
+            List<IProduct> products = new List<IProduct>()
+            { this.secondProduct.Object, this.thirdProduct.Object};
+
+            Assert.That(this.mockProductStock.Object.FindAllInRange(100, 400), Is.EqualTo(products));
+
+            List<IProduct> emptyCollection = new List<IProduct>();
+
+            Assert.That(this.mockProductStock.Object.FindAllInRange(10, 40), Is.EqualTo(emptyCollection));
+        }
+
+        [Test]
+        public void MockFindAllByPriceAhouldFindAllProductByPrice()
+        {
+            Mock<IProduct> samePriceProduct = new Mock<IProduct>();
+            samePriceProduct.Setup(p => p.Label).Returns("sameProductLabel");
+            samePriceProduct.Setup(p => p.Price).Returns(123M);
+            samePriceProduct.Setup(p => p.Quantity).Returns(21);
+
+            this.mockProductStock.Setup(ps => ps.FindAllByPrice(123))
+                .Returns(new List<IProduct>() { this.secondProduct.Object, samePriceProduct.Object });
+
+            List<IProduct> products = new List<IProduct>()
+            { this.secondProduct.Object, samePriceProduct.Object};
+
+            Assert.That(this.mockProductStock.Object.FindAllByPrice(123), Is.EqualTo(products));
+
+            List<IProduct> emptyCollection = new List<IProduct>();
+
+            Assert.That(this.mockProductStock.Object.FindAllByPrice(300), Is.EqualTo(emptyCollection));
+        }
+
+        [Test]
+        public void MockFindAllByQuantityShouldReturnAllProductByQuantity()
+        {
+            Mock<IProduct> samePriceProduct = new Mock<IProduct>();
+            samePriceProduct.Setup(p => p.Label).Returns("sameProductLabel");
+            samePriceProduct.Setup(p => p.Price).Returns(123M);
+            samePriceProduct.Setup(p => p.Quantity).Returns(21);
+
+            this.mockProductStock.Setup(ps => ps.FindAllByQuantity(21))
+                .Returns(new List<IProduct>() { this.secondProduct.Object, samePriceProduct.Object });
+
+            List<IProduct> products = new List<IProduct>()
+            { this.secondProduct.Object, samePriceProduct.Object};
+
+            Assert.That(this.mockProductStock.Object.FindAllByQuantity(21), Is.EqualTo(products));
+
+            List<IProduct> emptyCollection = new List<IProduct>();
+
+            Assert.That(this.mockProductStock.Object.FindAllByQuantity(4), Is.EqualTo(emptyCollection));
         }
     }
 }
---
+++ C# OOP/016.MockingAndTestDrivenDevelopment/INStock/Market.cs
@@ -0,0 +1,73 @@
+﻿using INStock.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INStock
+{
+    public class Market
+    {
+        private IProductStock productStock;
+
+        public Market(IProductStock productStock)
+        {
+            this.productStock = productStock;
+        }
+
+        public IProductStock ProductStock { get { return productStock; } }
+
+        public int CurrentProductCount()
+        {
+            return this.ProductStock.Count;
+        }
+
+        public IProduct this[int index] { get => this.ProductStock[index]; set => this.ProductStock[index] = value; }
+
+        public bool Contains(IProduct product)
+        {
+            return this.ProductStock.Contains(product);
+        }
+
+        public void Add(IProduct product)
+        {
+            this.ProductStock.Add(product);
+        }
+
+        public bool Remove(IProduct product)
+        {
+            return this.ProductStock.Remove(product);
+        }
+
+        public IProduct Find(int index)
+        {
+            return this.ProductStock.Find(index);
+        }
+
+        public IProduct FindByLabel(string label)
+        {
+            return this.ProductStock.FindByLabel(label);
+        }
+
+        public IProduct FindMostExpensiveProduct()
+        {
+            return this.ProductStock.FindMostExpensiveProduct();
+        }
+
+        public IEnumerable<IProduct> FindAllInRange(decimal lo, decimal hi)
+        {
+            return this.ProductStock.FindAllInRange(lo, hi);
+        }
+
+        public IEnumerable<IProduct> FindAllByPrice(decimal price)
+        {
+            return this.ProductStock.FindAllByPrice(price);
+        }
+
+        public IEnumerable<IProduct> FindAllByQuantity(int quantity)
+        {
+            return this.ProductStock.FindAllByQuantity(quantity);
+        }
+    }
+}
