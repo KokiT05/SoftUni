98c3be801ba3ff22c9d2fb622686db265dd5c75d
Konstantin Tinev <konstantintinev5@gmail.com>
2025-02-02 09:28:50 +0200

    Add new tasks from the topic: Exercise design patterns

---
+++ C# OOP/019.ExerciseDesignPatterns/02.Composite/CompositeGift.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Composite
+{
+    public class CompositeGift : GiftBase, IGiftOperations
+    {
+        private readonly ICollection<GiftBase> gifts;
+
+        public CompositeGift(string name, int price) : base(name, price)
+        {
+            gifts = new List<GiftBase>();
+        }
+
+        public void Add(GiftBase giftBase)
+        {
+            gifts.Add(giftBase);
+        }
+
+        public void Remove(GiftBase giftBase)
+        {
+            gifts.Remove(giftBase);
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            int total = 0;
+
+            Console.WriteLine($"{name} contains the following products with prices: ");
+
+            foreach (GiftBase gift in gifts)
+            {
+                total += gift.CalculateTotalPrice();
+            }
+
+            return total;
+        }
+    }
+}
---
+++ C# OOP/019.ExerciseDesignPatterns/02.Composite/IGiftOperations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Composite
+{
+    public interface IGiftOperations
+    {
+        void Add(GiftBase giftBase);
+
+        void Remove(GiftBase giftBase);
+    }
+}
